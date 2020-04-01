020daaf241baf2d55fde2daaa8ec102df6ff16fe
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2020-04-01 15:40:47 +0200

    setup shape production

--- shapes/config.py
+++ shapes/config.py
@@ -1,4 +1,7 @@
 from ntuple_processor.utils import Selection
+from ntuple_processor.utils import Cut
+from ntuple_processor.utils import Weight
+from ntuple_processor.variations import ReplaceCut
 
 # Base path to main ntuples
 ntuples_base = "/ceph/htautau/deeptau_02-20/2018/ntuples/"
@@ -6,7 +9,7 @@ ntuples_base = "/ceph/htautau/deeptau_02-20/2018/ntuples/"
 # No friend trees
 friends_base = []
 
-# Files associated to processes
+# File list
 files = {
     'singlemuon': [
         'SingleMuon_Run2018A_17Sep2018v2_13TeV_MINIAOD',
@@ -28,10 +31,42 @@ files = {
         'DY4JetsToLLM50_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_madgraph-pythia8_v1',
         'DYJetsToLLM10to50_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_madgraph-pythia8_v2',
         'DYJetsToLLM50_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_madgraph-pythia8_v1'
+        ],
+    'tt': [
+        'TTTo2L2Nu_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1',
+        'TTToHadronic_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1',
+        'TTToSemiLeptonic_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1'
+        ],
+    'vv': [
+        'WW_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_pythia8_v2',
+        'ZZ_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_pythia8_v2',
+        'WZ_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_pythia8_v3',
+        'STt-channelantitop4fInclusiveDecaysTuneCP5_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1',
+        'STt-channeltop4fInclusiveDecaysTuneCP5_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1',
+        'STtWantitop5finclusiveDecaysTuneCP5_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_ext1-v1',
+        'STtWtop5finclusiveDecaysTuneCP5_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_ext1-v1'
+        ],
+    'ggh': [
+        'GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2',
+        'GluGluHToTauTauHTXSFilterSTXS1p1Bin104to105M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1',
+        'GluGluHToTauTauHTXSFilterSTXS1p1Bin106M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2',
+        'GluGluHToTauTauHTXSFilterSTXS1p1Bin107to109M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1',
+        'GluGluHToTauTauHTXSFilterSTXS1p1Bin110to113M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2',
+        'GluGluHToTauTauM125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2'
+        ],
+    'qqh': [
+        'VBFHToTauTauHTXSFilterSTXS1p1Bin203to205M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1',
+        'VBFHToTauTauHTXSFilterSTXS1p1Bin206M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1',
+        'VBFHToTauTauHTXSFilterSTXS1p1Bin207to210M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1',
+        'VBFHToTauTauM125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_ext1-v1',
+        'ZHToTauTauM125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2',
+        'WminusHToTauTauM125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2',
+        'WplusHToTauTauM125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2'
         ]
     }
 
 # Selections
+
 channel = Selection(name = "mt",
                cuts = [
                        ("flagMETFilter == 1", "METFilter"),
@@ -79,9 +114,136 @@ dy = Selection(name = "dy",
             ]
         )
 
-
 ztt = Selection(name = "ztt",
         cuts = [
             ("gen_match_1==4 && gen_match_2==5", "ztt_cut")
             ]
         )
+
+zl = Selection(name = "zl",
+        cuts = [
+            ("!(gen_match_1==4 && gen_match_2==5) && !(gen_match_2 == 6)", "zl_cut")
+            ]
+        )
+
+zj = Selection(name = "zj",
+        cuts = [
+            ("gen_match_2 == 6", "zj_cut")
+            ]
+        )
+
+tt = Selection(name = "tt",
+        weights = [
+            ("generatorWeight", "generatorWeight"),
+            ("numberGeneratedEventsWeight", "numberGeneratedEventsWeight"),
+            ("(abs(crossSectionPerEventWeight - 380.1) < 0.1)*377.96 + (abs(crossSectionPerEventWeight - 87.31) < 0.1)*88.29 + (abs(crossSectionPerEventWeight - 364.4) < 0.1)*365.35", "crossSectionPerEventWeight"),
+            ("puweight", "puweight"),
+            ("idWeight_1*idWeight_2", "idweight"),
+            ("isoWeight_1*isoWeight_2", "isoweight"),
+            ("trackWeight_1*trackWeight_2", "trackweight"),
+            ("topPtReweightWeight", "topPtReweightWeight"),
+            ("(crossTriggerMCWeight_1*(crossTriggerMCWeight_1<10 && crossTriggerMCWeight_1>0.1)+(crossTriggerMCWeight_1>10 || crossTriggerMCWeight_1<0.1))*(pt_1<25) + (trigger_24_27_Weight_1*(pt_1>25))", "triggerweight"),
+            ("eleTauFakeRateWeight*muTauFakeRateWeight", "leptonTauFakeRateWeight"),
+            ("((gen_match_2 == 5)*0.90 + (gen_match_2 != 5))", "taubyIsoIdWeight"),
+            lumi_weight
+            ]
+        )
+
+ttt = Selection(name = "ttt",
+        cuts = [
+            ("gen_match_1==4 && gen_match_2==5", "ttt_cut")
+            ]
+        )
+
+ttl = Selection(name = "ttl",
+        cuts = [
+            ("!(gen_match_1==4 && gen_match_2==5) & !(gen_match_2 == 6)", "ttl_cut")
+            ]
+        )
+
+ttj = Selection(name = "ttj",
+        cuts = [
+            ("(gen_match_2 == 6 && gen_match_2 == 6)", "ttj_cut")
+            ]
+        )
+
+vv = Selection(name = "vv",
+        weights = [
+            ("generatorWeight", "generatorWeight"),
+            ("numberGeneratedEventsWeight", "numberGeneratedEventsWeight"),
+            ("puweight", "puweight"),
+            ("idWeight_1*idWeight_2", "idweight"),
+            ("isoWeight_1*isoWeight_2", "isoweight"),
+            ("trackWeight_1*trackWeight_2", "trackweight"),
+            ("(crossTriggerMCWeight_1*(crossTriggerMCWeight_1<10 && crossTriggerMCWeight_1>0.1)+(crossTriggerMCWeight_1>10 || crossTriggerMCWeight_1<0.1))*(pt_1<25) + (trigger_24_27_Weight_1*(pt_1>25))", "triggerweight"),
+            ("eleTauFakeRateWeight*muTauFakeRateWeight", "leptonTauFakeRateWeight"),
+            ("((gen_match_2 == 5)*0.90 + (gen_match_2 != 5))", "taubyIsoIdWeight"),
+            ("crossSectionPerEventWeight", "crossSectionPerEventWeight"),
+            lumi_weight
+            ]
+        )
+
+vvt = Selection(name = "vvt",
+        cuts = [
+            ("gen_match_1==4 && gen_match_2==5", "vvt_cut")
+            ]
+        )
+vvl = Selection(name = "vvl",
+        cuts = [
+            ("!(gen_match_1==4 && gen_match_2==5) & !(gen_match_2 == 6)", "vvl_cut")
+            ]
+        )
+
+vvj = Selection(name = "vvj",
+        cuts = [
+            ("(gen_match_2 == 6 && gen_match_2 == 6)", "vvj_cut")
+            ]
+        )
+
+htt = Selection(name = "htt",
+        weights = [
+            ("generatorWeight", "generatorWeight"),
+            ("puweight", "puweight"),
+            ("idWeight_1*idWeight_2", "idweight"),
+            ("isoWeight_1*isoWeight_2", "isoweight"),
+            ("trackWeight_1*trackWeight_2", "trackweight"),
+            ("(crossTriggerMCWeight_1*(crossTriggerMCWeight_1<10 && crossTriggerMCWeight_1>0.1)+(crossTriggerMCWeight_1>10 || crossTriggerMCWeight_1<0.1))*(pt_1<25) + (trigger_24_27_Weight_1*(pt_1>25))", "triggerweight"),
+            ("eleTauFakeRateWeight*muTauFakeRateWeight", "leptonTauFakeRateWeight"),
+            ("((gen_match_2 == 5)*0.90 + (gen_match_2 != 5))", "taubyIsoIdWeight"),
+            lumi_weight
+            ]
+        )
+
+ggh = Selection(name = "ggh",
+        weights = [
+            ("ggh_NNLO_weight", "gghNNLO"),
+            ("1.01", "bbh_inclusion_weight"),
+            ("(((htxs_stage1p1cat==100||htxs_stage1p1cat==102||htxs_stage1p1cat==103)*crossSectionPerEventWeight*numberGeneratedEventsWeight+"
+             "(htxs_stage1p1cat==101)*2.09e-8+"
+             "(htxs_stage1p1cat==104||htxs_stage1p1cat==105)*4.28e-8+"
+             "(htxs_stage1p1cat==106)*1.39e-8+"
+             "(htxs_stage1p1cat>=107&&htxs_stage1p1cat<=109)*4.90e-8+"
+             "(htxs_stage1p1cat>=110&&htxs_stage1p1cat<=113)*9.69e-9"
+             ")*(abs(crossSectionPerEventWeight - 0.00538017) > 1e-5) + numberGeneratedEventsWeight*crossSectionPerEventWeight*(abs(crossSectionPerEventWeight - 0.00538017) < 1e-5))", "ggh_stitching_weight")
+            ],
+        cuts = [
+            ("(htxs_stage1p1cat>=100)&&(htxs_stage1p1cat<=113)", "htxs_cut")
+            ]
+        )
+
+qqh = Selection(name = "qqh",
+        weights = [
+            ("((htxs_stage1p1cat>=200&&htxs_stage1p1cat<=202)||abs(crossSectionPerEventWeight-0.04774)<0.001||abs(crossSectionPerEventWeight-0.052685)<0.001||abs(crossSectionPerEventWeight-0.03342)<0.001)*crossSectionPerEventWeight*numberGeneratedEventsWeight+(abs(crossSectionPerEventWeight-0.04774)>=0.001&&abs(crossSectionPerEventWeight-0.052685)>=0.001&&abs(crossSectionPerEventWeight-0.03342)>=0.001)*("
+             "(htxs_stage1p1cat>=203&&htxs_stage1p1cat<=205)*9.41e-9+"
+             "(htxs_stage1p1cat==206)*8.52e-9+"
+             "(htxs_stage1p1cat>=207&&htxs_stage1p1cat<=210)*1.79e-8"
+             ")", "qqh_stitching_weight")
+            ],
+        cuts = [
+            ("(htxs_stage1p1cat>=200)&&(htxs_stage1p1cat<=210)", "htxs_cut")
+            ]
+        )
+
+# Variations
+
+same_sign = ReplaceCut("same_sign", "os", Cut("q_1*q_2>0", "ss"))
--- shapes/shapes.py
+++ shapes/shapes.py
@@ -34,18 +34,43 @@ def main():
 
     # Define nominal units
     units = {}
+
     units['data'] = Unit(
             dataset_from_artusoutput('data', cfg.files['singlemuon'], 'mt_nominal', cfg.ntuples_base, cfg.friends_base),
             [cfg.channel], hists)
+
     units['w'] = Unit(
             dataset_from_artusoutput('w', cfg.files['wjets'], 'mt_nominal', cfg.ntuples_base, cfg.friends_base),
             [cfg.channel, cfg.w], hists)
+
     dy_dataset = dataset_from_artusoutput('dy', cfg.files['dy'], 'mt_nominal', cfg.ntuples_base, cfg.friends_base)
     units['ztt'] = Unit(dy_dataset,[cfg.channel, cfg.dy, cfg.ztt], hists)
+    units['zl'] = Unit(dy_dataset,[cfg.channel, cfg.dy, cfg.zl], hists)
+    units['zj'] = Unit(dy_dataset,[cfg.channel, cfg.dy, cfg.zj], hists)
+
+    tt_dataset = dataset_from_artusoutput('tt', cfg.files['tt'], 'mt_nominal', cfg.ntuples_base, cfg.friends_base)
+    units['ttt'] = Unit(tt_dataset,[cfg.channel, cfg.tt, cfg.ttt], hists)
+    units['ttl'] = Unit(tt_dataset,[cfg.channel, cfg.tt, cfg.ttl], hists)
+    units['ttj'] = Unit(tt_dataset,[cfg.channel, cfg.tt, cfg.ttj], hists)
+
+
+    vv_dataset = dataset_from_artusoutput('vv', cfg.files['vv'], 'mt_nominal', cfg.ntuples_base, cfg.friends_base)
+    units['vvt'] = Unit(vv_dataset,[cfg.channel, cfg.vv, cfg.vvt], hists)
+    units['vvl'] = Unit(vv_dataset,[cfg.channel, cfg.vv, cfg.vvl], hists)
+    units['vvj'] = Unit(vv_dataset,[cfg.channel, cfg.vv, cfg.vvj], hists)
+
+    units['ggh'] = Unit(
+            dataset_from_artusoutput('ggh', cfg.files['ggh'], 'mt_nominal', cfg.ntuples_base, cfg.friends_base),
+            [cfg.channel, cfg.htt, cfg.ggh], hists)
+
+    units['qqh'] = Unit(
+            dataset_from_artusoutput('qqh', cfg.files['qqh'], 'mt_nominal', cfg.ntuples_base, cfg.friends_base),
+            [cfg.channel, cfg.htt, cfg.qqh], hists)
 
     # Book units with variations
     um = UnitManager()
-    um.book([units[name] for name in ['data', 'w', 'ztt']])
+    um.book([units[name] for name in ['data', 'w', 'ztt', 'zl', 'zj', 'ttt', 'ttl', 'ttj', 'vvt', 'vvl', 'vvj', 'ggh', 'qqh']])
+    um.book([units[name] for name in ['data', 'zl', 'zj', 'w', 'ttt', 'ttj', 'ttl', 'vvt', 'vvj', 'vvl']], [cfg.same_sign])
 
     # Optimize graphs
     g_manager = GraphManager(um.booked_units)
@@ -54,7 +79,7 @@ def main():
 
     # Run computations
     r_manager = RunManager(graphs)
-    r_manager.run_locally('shapes.root', nworkers=12, nthreads=1)
+    r_manager.run_locally('shapes.root', nworkers=8, nthreads=2)
 
 
 if __name__ == "__main__":
