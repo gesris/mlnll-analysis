8c57be174daf8e1a12fb784957728c1bbc336ce4
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2020-04-06 11:09:37 +0200

    new path to own root

--- utils/setup_lcg.sh
+++ utils/setup_lcg.sh
@@ -3,7 +3,7 @@
 # TODO: Still still links to our local build, to be fixed as soon as ROOT 6.20 is on LCG
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
-source /home/wunsch/workspace/root/build_/bin/thisroot.sh
+source /home/wunsch/workspace/root/build_native/bin/thisroot.sh
 
 export PYTHONPATH=$PWD:$PYTHONPATH
 export PYTHONPATH=$PWD/Dumbledraw:$PYTHONPATH
