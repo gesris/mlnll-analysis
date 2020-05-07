c03c389b5c92bc3e0009ef995f2bb32b4d9f47f7
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2020-05-07 16:35:37 +0200

    add ML fit script

---
+++ fit/signal_strength.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source utils/setup_cmssw.sh
+
+WORKDIR=$1
+
+pushd $WORKDIR
+combine -M FitDiagnostics \
+        -n .MLNLL \
+        -m 125 \
+        -d workspace.root \
+        --robustFit 1
