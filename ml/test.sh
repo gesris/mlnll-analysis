7684b63d5f3f24313181ae07b698796d78e26fc6
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2020-05-14 14:36:25 +0200

    reenable tests for both folds

--- ml/test.sh
+++ ml/test.sh
@@ -4,7 +4,8 @@ source utils/setup_lcg.sh
 
 WORKDIR=$1
 
-#python ml/test_confusion.py $WORKDIR 0
-#python ml/test_confusion.py $WORKDIR 1
+python ml/test_confusion.py $WORKDIR 0
+python ml/test_confusion.py $WORKDIR 1
 
 python ml/test_taylor.py $WORKDIR 0
+python ml/test_taylor.py $WORKDIR 1
