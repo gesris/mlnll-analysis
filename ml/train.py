175ae86bb8fe231e1b054316fe28f6c85e7c6c77
gesris <gessi.risto@gmx.de>
2020-05-28 16:23:42 +0200

    automatically generated arbitrary commit message for convenience

--- ml/train.py
+++ ml/train.py
@@ -146,7 +146,8 @@ def main(args):
     # Create model
     x_ph = tf.placeholder(tf.float32)
     logits, f = model(x_ph, len(cfg.ml_variables), len(cfg.ml_classes), args.fold)
-    print("F = ", f)
+    print("\n")
+    tf.Print(f)
 
     # Add CE loss
     y_ph = tf.placeholder(tf.float32)
