80ebcc7b837b29d9ccc09420d2e3047225994e01
gesris <gessi.risto@gmx.de>
2020-06-08 14:18:29 +0200

    changed ml_classes to single nll class and analysis categories to single cat

--- utils/config.py
+++ utils/config.py
@@ -11,7 +11,8 @@ ntuples_base = path.join(basepath, 'ntuples')
 
 # Friend trees
 friends_base = [path.join(basepath, 'friends', f) for f in ['TauTriggers', 'SVFit']]
-ml_score_base = ['/home/wunsch/workspace/mlnll-analysis/output_ml/MLScores']
+#ml_score_base = ['/home/wunsch/workspace/mlnll-analysis/output_ml/MLScores']
+ml_score_base = ['/home/gristo/mlnll-analysis/test0/MLScores']
 
 # File list
 files = {
@@ -300,12 +301,15 @@ control_variables = list(binning.keys())
 
 ml_variables = ['njets', 'nbtag', 'ptvis', 'pt_tt', 'mjj', 'jdeta', 'dijetpt', 'met', 'm_vis', 'm_sv_puppi']
 ml_weight = 'training_weight'
-ml_classes = ['htt', 'ztt', 'w', 'tt']
+#ml_classes = ['htt', 'ztt', 'w', 'tt']
+ml_classes = 'nll'
 
 # Analysis categories
 
 # leere selektion eintragen, da keine kategorien mehr notwendig sind -> HIER NUR EINE KLASSE
-analysis_categories = {c + '_cat': Selection(name = c + '_cat', cuts=[('ml_index == ' + str(i), 'ml_index')]) \
+#analysis_categories = {c + '_cat': Selection(name = c + '_cat', cuts=[('ml_index == ' + str(i), 'ml_index')]) \
+#                      for i, c in enumerate(ml_classes)}
+analysis_categories = {c + '_cat': Selection(name = c + '_cat') \
                       for i, c in enumerate(ml_classes)}
 #analysis_binning = [0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
 analysis_binning = [0.0, 0.5, 1.0]
