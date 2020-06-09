3f88ad2c2190ffb683b917e4ab38d273a632f0db
gesris <gessi.risto@gmx.de>
2020-06-09 09:16:38 +0200

    test

--- utils/config.py
+++ utils/config.py
@@ -302,15 +302,15 @@ control_variables = list(binning.keys())
 ml_variables = ['njets', 'nbtag', 'ptvis', 'pt_tt', 'mjj', 'jdeta', 'dijetpt', 'met', 'm_vis', 'm_sv_puppi']
 ml_weight = 'training_weight'
 ml_classes = ['htt', 'ztt', 'w', 'tt']
-ml_output = ['nll']
+#ml_output = ['nll']
 
 # Analysis categories
 
 # leere selektion eintragen, da keine kategorien mehr notwendig sind -> HIER NUR EINE KLASSE
-#analysis_categories = {c + '_cat': Selection(name = c + '_cat', cuts=[('ml_index == ' + str(i), 'ml_index')]) \
-#                      for i, c in enumerate(ml_classes)}
-analysis_categories = {c + '_cat': Selection(name = c + '_cat') \
-                      for i, c in enumerate(ml_output)}
+analysis_categories = {c + '_cat': Selection(name = c + '_cat', cuts=[('ml_index == ' + str(i), 'ml_index')]) \
+                      for i, c in enumerate(ml_classes)}
+#analysis_categories = {c + '_cat': Selection(name = c + '_cat') \
+#                      for i, c in enumerate(ml_output)}
 #analysis_binning = [0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
 analysis_binning = [0.0, 0.5, 1.0]
 analysis_variable = 'ml_score'
