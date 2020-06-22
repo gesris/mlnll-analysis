abf096b6d6e417e9f41e8f25fe12db60590a9d26
gesris <gessi.risto@gmx.de>
2020-06-22 14:29:25 +0200

    automatically generated arbitrary commit message for convenience

--- utils/config.py
+++ utils/config.py
@@ -12,7 +12,7 @@ ntuples_base = path.join(basepath, 'ntuples')
 # Friend trees
 friends_base = [path.join(basepath, 'friends', f) for f in ['TauTriggers', 'SVFit']]
 #ml_score_base = ['/home/wunsch/workspace/mlnll-analysis/output_ml/MLScores']
-ml_score_base = ['/work/gristo/mlnll-analysis/output/16_bins_2/MLScores']
+ml_score_base = ['/work/gristo/mlnll-analysis/output/16_bins_3/MLScores']
 
 # File list
 files = {
