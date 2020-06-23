c3f720afaddae8cb266a7ae1f75adddfc9c4d6d1
gesris <gessi.risto@gmx.de>
2020-06-23 13:43:36 +0200

    removed hardcoded paths

--- shapes/condor_loop.sh
+++ shapes/condor_loop.sh
@@ -3,6 +3,7 @@
 # While loop counting sum of elements in directory for automatization
 
 WORKDIR=$1
+ANALYSISDIR=$2
 
 run=true
 step=0
@@ -10,7 +11,7 @@ step=0
 while $run; do
     sleep 120s
     clear
-    if [ $(ls -l /work/gristo/mlnll-analysis/$WORKDIR/shapes_jobs/err | grep err | wc -l) =  191 ]; then
+    if [ $(ls -l $ANALYSISDIR/$WORKDIR/shapes_jobs/err | grep err | wc -l) =  191 ]; then
         printf "\n\n## --------------  All jobs done  --------------- ##\n\n"
         run=false
         break
@@ -18,7 +19,7 @@ while $run; do
 
     if [ $(condor_q | grep HOLD | wc -l) = 1 ]; then
         printf "\n\n## ---------  Releasing jobs from HOLD  --------- ##\n\n"
-        sh /work/gristo/mlnll-analysis/condor_release.sh
+        sh $ANALYSISDIR/condor_release.sh
     fi
 
     condor_q | head -5
