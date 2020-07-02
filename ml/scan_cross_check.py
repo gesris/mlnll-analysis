d5c5f5e54553f1e523dc5cfc284f8f2d49598879
gesris <gessi.risto@gmx.de>
2020-07-02 15:56:26 +0200

    bugfixes

--- ml/scan_cross_check.py
+++ ml/scan_cross_check.py
@@ -69,7 +69,7 @@ def main():
     def create_dnll_file(mu0, x, Htt, Ztt, W, ttbar):
         mu1 = tf.constant(x, dtype=tf.float32)
         for i in tqdm(range(0, len(x))):
-            d_value = tf.Session().run(2 * (nll_value(mu1[i], Htt, Ztt, W, ttbar) - nll_value(mu0, Htt, Ztt, W, ttbar)))
+            d_value = [tf.Session().run(2 * (nll_value(mu1[i], Htt, Ztt, W, ttbar) - nll_value(mu0, Htt, Ztt, W, ttbar)))]
             #with open(r'./dnll_value_list.csv', 'a') as file:
             #    writer = csv.writer(file)
             #    writer.writerow(d_value)
