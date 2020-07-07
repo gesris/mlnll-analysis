5a1683bfd38f76653c97ad9abbd6d159556fff88
gesris <gessi.risto@gmx.de>
2020-07-07 17:57:57 +0200

    bugfixes

--- ml/test_model.py
+++ ml/test_model.py
@@ -88,7 +88,7 @@ def main(args):
     y_array = np.array(y)
 
     logger.info("\n\nX: {}".format(x))
-    logger.info("\n\nX: {}".format(x[:-3]))
+    logger.info("\n\nX: {}".format(x[-3:]))
 
     logger.info("\n\nTTBAR LABELS: {}".format(y_array[:, 3]))
     logger.info("\n\nTTBAR WEIGHTS: {}".format(w[y_array[:, 3] == 1]))
