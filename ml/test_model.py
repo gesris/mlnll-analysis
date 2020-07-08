f675ac023073c48001f892d8b629bc26a364d2e4
gesris <gessi.risto@gmx.de>
2020-07-08 13:48:28 +0200

    bugfixes

--- ml/test_model.py
+++ ml/test_model.py
@@ -92,11 +92,11 @@ def main(args):
     logger.info("\n\nX TTBAR: {}\nLength: {}".format(x_preproc[y_array[:, 3] == 1], len(x_preproc[y_array[:, 3] == 1])))
     logger.info("\n\nTTBAR WEIGHTS: {}\nLength: {}".format(w[y_array[:, 3] == 1], len(w[y_array[:, 3] == 1])))
     logger.info("\n\nTTBAR SUMWEIGHTS: {}".format(np.sum(w[y_array[:, 3] == 1])))
-    x_weighted = x
+    x_weighted = x_preproc
     #for i in range(1, 4):
     #    test[:, -1 * i] = test[:, -1 * i] * w[y_array[:, 3] == 1]
     summe = 0.
-    for i in range(7, 10):
+    for i in range(0, 2):
         x_weighted[:, i] = x_weighted[:, i] * w
         #logger.info("\n\nXxW: {}".format(test[y_array[:, 3] == 1][:, i]))
         summe += np.sum(x_weighted[y_array[:, 3] == 1][:, i])
