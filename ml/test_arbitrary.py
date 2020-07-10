ce6e367e2701b71131219c11987303e84e235675
gesris <gessi.risto@gmx.de>
2020-07-10 13:59:30 +0200

    bugfixes

--- ml/test_arbitrary.py
+++ ml/test_arbitrary.py
@@ -3,20 +3,32 @@ import ROOT
 path = '/work/gristo/mlnll-analysis/output/32_bins_shapes/cmb/common/htt_input_2018.root'
 f = ROOT.TFile(path)
 d = f.Get('htt_mt_0_2018')
-procs = {}
-hdata = []
-hcount = []
+nbins = 32
+
+yields = {}
+bincounts = {}
+
+hnames = []
+hyields = []
+
 for key in d.GetListOfKeys():
     name = key.GetName()
     if 'ggH125' in name:
         h = d.Get(name)
-        procs[name] = [h.Integral()]
-        h.Draw("HIST SAME")
 
-        hdata.append(name)
-        hcount.append(procs[name][0])
+        yields[name] = [h.Integral()]
 
-for i in range(0, len(hdata)):
-    print("Name: {},        Yield: {:.3f}".format(hdata[i], hcount[i]))
+        hnames.append(name)
+        hyields.append(yields[name][0])
+        bincounts[name] = [h.GetBinContent(i + 1) for i in range(nbins)]
+
+hist = []
+
+for i in range(nbins):
+    hist.append(sum([bincounts[name][i] for name in hnames]))
+print(hist)
+
+for i in range(0, len(hnames)):
+    print("Name: {},        Yield: {:.3f}".format(hnames[i], hyields[i]))
 
 
