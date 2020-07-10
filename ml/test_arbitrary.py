fa622e8d8f7f02045f93a3e3efe8498e1892ffe8
gesris <gessi.risto@gmx.de>
2020-07-10 12:18:33 +0200

    bugfixes

--- ml/test_arbitrary.py
+++ ml/test_arbitrary.py
@@ -4,18 +4,19 @@ path = '/work/gristo/mlnll-analysis/output/32_bins_shapes/cmb/common/htt_input_2
 f = ROOT.TFile(path)
 d = f.Get('htt_mt_0_2018')
 procs = {}
+hdata = []
 hcount = []
 for key in d.GetListOfKeys():
     name = key.GetName()
     if 'ggH125_THU_ggH_' in name:
         h = d.Get(name)
         procs[name] = [h.Integral()]
+
+        hdata.append(procs[name])
         hcount.append(procs[name][0])
-#hdata = []
 
-
-#hdata.append(procs[name])
-
-print(hcount)
+for i in range(0, len(hdata)):
+    print(hdata[i])
+    print(hcount[i])
 
 
