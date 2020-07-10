4399577f87ca0735fd85be6a8e1a23182ac6fe93
gesris <gessi.risto@gmx.de>
2020-07-10 12:04:27 +0200

    bugfixes

--- ml/test_arbitrary.py
+++ ml/test_arbitrary.py
@@ -8,11 +8,11 @@ for key in d.GetListOfKeys():
     name = key.GetName()
     if 'qqH125_THU_qqH_JET01' in name:
         h = d.Get(name)
-        print(h)
-        procs[name] = [h.Integrate()]
-hdata = []
+        print(h.Integrate())
+        #procs[name] = [h.Integrate()]
+#hdata = []
 #hcount = []
 
-hdata.append(procs[name])
+#hdata.append(procs[name])
 #hcount.append(procs[name][0])
 
