65ec5ef903dabb7a949c6fd3e00b2cebb2e32368
gesris <gessi.risto@gmx.de>
2020-07-16 12:01:36 +0200

    bugfixes

--- ml/check_systematics.py
+++ ml/check_systematics.py
@@ -51,8 +51,8 @@ def diff_hists():
 array = []
 path_ = '/ceph/htautau/deeptau_02-20/2018/ntuples/GluGluHToTauTauM125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2/GluGluHToTauTauM125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2.root'
 file_ = ROOT.TFile(path_)
-tree_ = file_.Get("mt_nominal/ntuple/THU_ggH_Mig01")
-print(tree_.GetName())
+tree_ = file_.Get("mt_nominal/ntuple")
+print(tree_.GetBranch("THU_ggH_Mig01"))
 
 
 
