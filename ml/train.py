ca717bf17d555b3ab725aba960b605bb75d578d1
gesris <gessi.risto@gmx.de>
2020-07-16 15:58:22 +0200

    bugfixes

--- ml/train.py
+++ ml/train.py
@@ -68,7 +68,7 @@ def build_dataset(path, classes, fold, make_categorical=True, use_class_weights=
         d_sys = tree2numpy(path_sys, c, columns)
         xs.append(np.vstack([np.array(d[k], dtype=np.float32) for k in cfg.ml_variables]).T)
         w = np.array(d[cfg.ml_weight], dtype=np.float32)
-        mig01 = np.array(d["THU_ggH_Mig01"], dtype=np.float32)
+        mig01 = np.array(d_sys["THU_ggH_Mig01"], dtype=np.float32)
         ws.append(w)
         ys.append(np.ones(d[cfg.ml_weight].shape) * i)
         mig01s.append(mig01)
