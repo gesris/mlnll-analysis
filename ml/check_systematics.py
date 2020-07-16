37ab629699f422ae2661dfd35a6ea5419b421e38
gesris <gessi.risto@gmx.de>
2020-07-16 12:14:46 +0200

    bugfixes

--- ml/check_systematics.py
+++ ml/check_systematics.py
@@ -55,7 +55,7 @@ tree_ = file_.Get("mt_nominal/ntuple")
 branch_ = tree_.GetBranch("THU_ggH_Mig01")
 
 file2_ = ROOT.RDataFrame("mt_nominal/ntuple", path_)
-print(file2_.AsNumpy("THU_ggH_Mig01"))
+print(file2_.AsNumpy())
 
 
 
