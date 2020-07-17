bd966b7191c3936975a931bb4066d7a46973fbc8
gesris <gessi.risto@gmx.de>
2020-07-17 10:19:57 +0200

    adding output messages

--- ml.sh
+++ ml.sh
@@ -5,10 +5,13 @@ WORKDIR=$1
 mkdir -p $WORKDIR
 
 # Create training dataset
+echo "  Create Training Dataset"
 ml/dataset.sh $WORKDIR
 
 # Train model
+echo "  Train Model"
 ml/train.sh $WORKDIR
 
 # Validate model
+echo "  Validate Model"
 ml/test_model.sh $WORKDIR
