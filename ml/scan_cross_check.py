54f69b0b87cf01548503a22b82b0bafb60e90f5f
gesris <gessi.risto@gmx.de>
2020-07-20 00:32:21 +0200

    bugfixes

--- ml/scan_cross_check.py
+++ ml/scan_cross_check.py
@@ -83,13 +83,15 @@ def main():
 
         # write new data into file
         mu1 = tf.constant(x, dtype=tf.float64)
+        session = tf.Session()
+        session.run([tf.global_variables_initializer()])
         for i in tqdm(range(0, len(x))):
             # NOSYS
             nll_val_nosys, _ = nll_value(mu0, Htt, Ztt, W, ttbar, Htt_up, Htt_down)
             _, nll_val_sys = nll_value(mu0, Htt, Ztt, W, ttbar, Htt_up, Htt_down)
             nll_val_nosys_var, _  = nll_value(mu1[i], Htt, Ztt, W, ttbar, Htt_up, Htt_down)
             _, nll_val_sys_var  = nll_value(mu1[i], Htt, Ztt, W, ttbar, Htt_up, Htt_down)
-            d_value_nosys, d_value_sys = tf.Session().run([2 * (nll_val_nosys_var - nll_val_nosys), 2 * (nll_val_sys_var - nll_val_sys)])
+            d_value_nosys, d_value_sys = session.run([2 * (nll_val_nosys_var - nll_val_nosys), 2 * (nll_val_sys_var - nll_val_sys)])
             d_value = [d_value_nosys, d_value_sys]
             with open(os.path.join(args.workdir, 'model_fold{}/dnll_value_list_nosys.csv'.format(args.fold)), "ab") as file:
                 np.savetxt(file, d_value[0])
