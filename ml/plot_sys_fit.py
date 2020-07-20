f07c47e77ec6cb2c1bfdf1426cca1c12e0327c65
gesris <gessi.risto@gmx.de>
2020-07-20 14:05:50 +0200

    new programm to compare fits with and without systematics

---
+++ ml/plot_sys_fit.py
@@ -0,0 +1,17 @@
+import os
+import argparse
+
+import numpy as np
+np.random.seed(1234)
+
+import ROOT
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+path = '/home/gristo/mlnll-analysis/output/4_bins_wsys_shapes/higgsCombine.Scan.MultiDimFit.mH125.root'
+df = ROOT.RDataFrame('limit', path)
+data = df.AsNumpy(['r', 'deltaNLL'])
+
+print(data)
+#plt.plot(data['r'], 2 * data['deltaNLL'], '+')
+#plt.savefig('plot.png', bbox_inches='tight')
