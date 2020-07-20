f52b45b70ddf0a935af21961b0c480a43fa922d7
gesris <gessi.risto@gmx.de>
2020-07-20 14:27:09 +0200

    bugfixes

--- ml/plot_sys_fit.py
+++ ml/plot_sys_fit.py
@@ -12,18 +12,25 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def main(args):
+def save_to_csv(args):
+    # Load from ROOT file
     path = os.path.join(args.workdir, 'higgsCombine.Scan.MultiDimFit.mH125.root')
-    #path = '/home/gristo/mlnll-analysis/output/4_bins_wsys_shapes/higgsCombine.Scan.MultiDimFit.mH125.root'
     df = ROOT.RDataFrame('limit', path)
     data = df.AsNumpy(['r', 'deltaNLL'])
-    for i in range(len(data['r'])):
-        print(data['r'][i])
-    #plt.plot(data['r'], 2 * data['deltaNLL'], '+')
-    #plt.savefig('plot.png', bbox_inches='tight')
+
+    # Save as .csv
+    open(os.path.join(args.workdir, 'scan_data_{}.csv'.format(args.method)), "w").close()
+    with open(os.path.join(args.workdir, 'scan_data_{}.csv'.format(args.method)), "ab") as file:
+        np.savetxt(file, data['r'])
+        np.savetxt(file, data['deltaNLL'])
+
+def main(args):
+    save_to_csv(args)
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('workdir', help='Working directory for outputs')
+    parser.add_argument('method', help='Method with or without Sys')
     args = parser.parse_args()
     main(args)
