5bd08ca08d31704f1cb4088e55becf61090138c8
gesris <gessi.risto@gmx.de>
2020-07-20 14:11:11 +0200

    bugfixes

--- ml/plot_sys_fit.py
+++ ml/plot_sys_fit.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 path = '/home/gristo/mlnll-analysis/output/4_bins_wsys_shapes/higgsCombine.Scan.MultiDimFit.mH125.root'
 df = ROOT.RDataFrame('limit', path)
 data = df.AsNumpy(['r', 'deltaNLL'])
-
-print(data['r'][2])
+for i in len(data['r'][:]):
+    print(data['r'][i])
 #plt.plot(data['r'], 2 * data['deltaNLL'], '+')
 #plt.savefig('plot.png', bbox_inches='tight')
