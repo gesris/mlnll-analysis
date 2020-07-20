0947d1aaefbc997dd76b98f36b4c22738b6fc065
gesris <gessi.risto@gmx.de>
2020-07-20 14:18:12 +0200

    bugfixes

--- ml/plot_sys_fit.py
+++ ml/plot_sys_fit.py
@@ -4,14 +4,26 @@ import argparse
 import numpy as np
 np.random.seed(1234)
 
+import csv
+from csv import reader
+
 import ROOT
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-path = '/home/gristo/mlnll-analysis/output/4_bins_wsys_shapes/higgsCombine.Scan.MultiDimFit.mH125.root'
-df = ROOT.RDataFrame('limit', path)
-data = df.AsNumpy(['r', 'deltaNLL'])
-for i in range(len(data['r'])):
-    print(data['r'][i])
-#plt.plot(data['r'], 2 * data['deltaNLL'], '+')
-#plt.savefig('plot.png', bbox_inches='tight')
+
+def main():
+    path = os.path.join(args.workdir, 'higgsCombine.Scan.MultiDimFit.mH125.root')
+    #path = '/home/gristo/mlnll-analysis/output/4_bins_wsys_shapes/higgsCombine.Scan.MultiDimFit.mH125.root'
+    df = ROOT.RDataFrame('limit', path)
+    data = df.AsNumpy(['r', 'deltaNLL'])
+    for i in range(len(data['r'])):
+        print(data['r'][i])
+    #plt.plot(data['r'], 2 * data['deltaNLL'], '+')
+    #plt.savefig('plot.png', bbox_inches='tight')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('workdir', help='Working directory for outputs')
+    args = parser.parse_args()
+    main(args)
