222730a7957e7f05bf350a08ca025d49008321b9
gesris <gessi.risto@gmx.de>
2020-07-22 17:33:52 +0200

    bugfixes

--- ml/plt_sys_fit2.py
+++ ml/plt_sys_fit2.py
@@ -24,7 +24,7 @@ def load_from_csv(workdir, method):
                 lines.append(float(elements[i]))
             counts.append(lines)
     r = counts[0]
-    deltaNLL = counts[1]
+    deltaNLL = counts[1] * 2
     return(r, deltaNLL)
 
 def main(args):
