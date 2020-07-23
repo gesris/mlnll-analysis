2a0bf3fc2fd919f0cd26bb16926b5e80a5d0d88b
gesris <gessi.risto@gmx.de>
2020-07-23 11:48:12 +0200

    changed method to interpolation

--- ml/scan_cross_check.py
+++ ml/scan_cross_check.py
@@ -1,17 +1,23 @@
 import os
 import argparse
+import csv
+from csv import reader
 
 import numpy as np
 np.random.seed(1234)
+from scipy import interpolate
 import tensorflow.compat.v1 as tf
 import tensorflow_probability as tfp
 tf.disable_v2_behavior()
 tf.set_random_seed(1234)
 
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+matplotlib.rc("font", size=16, family="serif")
+
 from tqdm import tqdm
-import csv
-from csv import reader
+
 
 import logging
 logger = logging.getLogger('')
@@ -29,8 +35,6 @@ def setup_logging(output_file, level=logging.DEBUG):
 
 
 def main():
-    mu = tf.constant(1.0, tf.float64)
-
     def load_hists():
         with open(os.path.join(args.workdir, 'model_fold{}/hists.csv'.format(args.fold)), 'rU') as file:
             counts = []
@@ -45,7 +49,9 @@ def main():
         W = tf.constant(counts[2], tf.float64)
         ttbar = tf.constant(counts[3], tf.float64)
         return(Htt, Ztt, W, ttbar)
-    
+
+    Htt, Ztt, W, ttbar = load_hists()
+    mu = tf.constant(1.0, tf.float64)
 
     def nll_value(mu, Htt, Ztt, W, ttbar):
         zero = tf.constant(0, tf.float64)
@@ -63,34 +69,22 @@ def main():
             nll_statsonly -= tfp.distributions.Poisson(tf.maximum(exp, epsilon)).log_prob(tf.maximum(obs, epsilon))
         return nll_statsonly
 
+    ## Calculate DNLL
+    x = np.linspace(0.0, 2.0, 30)
+    dnll_array = []
+    for i in x:
+        mu1 = tf.constant(i, tf.float64)
+        dnll_array.append(- 2 * (nll_value(mu1, Htt, Ztt, W, ttbar) - nll_value(mu, Htt, Ztt, W, ttbar)))
 
-    def create_dnll_file(mu0, x, Htt, Ztt, W, ttbar):
-        # empty file
-        open(os.path.join(args.workdir, 'model_fold{}/dnll_value_list.csv'.format(args.fold)), "w").close()
+    ## Interpolate DNLL data
+    f_dnll_array = interpolate.UnivariateSpline(x, dnll_array, s=0)
+    x_new = np.arange(0.0, 2.0, 0.02)
 
-        # write new data into file
-        mu1 = tf.constant(x, dtype=tf.float64)
-        for i in tqdm(range(0, len(x))):
-            d_value = [tf.Session().run(2 * (nll_value(mu1[i], Htt, Ztt, W, ttbar) - nll_value(mu0, Htt, Ztt, W, ttbar)))]
-            with open(os.path.join(args.workdir, 'model_fold{}/dnll_value_list.csv'.format(args.fold)), "ab") as file:
-                np.savetxt(file, d_value)
-
-
-    def scan_from_file(x):
-        with open(os.path.join(args.workdir, 'model_fold{}/dnll_value_list.csv'.format(args.fold)), 'r') as file:
-            diff = []
-            sigma_left_list = []
-            for i, d_value_ in enumerate(reader(file)):
-                d_value = float(d_value_[0])
-                diff.append(d_value)
-        return diff
-                
-
-    def second_derivative(mu, Htt, Ztt, W, ttbar):
-        return tf.Session().run(tf.gradients(tf.gradients(nll_value(mu, Htt, Ztt, W, ttbar), mu), mu))
-
-
-    Htt, Ztt, W, ttbar = load_hists()
+    y_target = 1
+    y_reduced = np.array(dnll_array) - y_target
+    freduced = interpolate.UnivariateSpline(x, y_reduced, s=0)
+    constraints_xval = freduced.roots()
+    constraints = [1 - constraints_xval[0], constraints_xval[1] - 1]
 
     ####
     #### Create data for parabola fit
@@ -99,44 +93,38 @@ def main():
     def f(x, a, b):
         return a*(x-b)**2
 
-    x = np.linspace(0.0, 2.0, 100)
+    def second_derivative(mu, Htt, Ztt, W, ttbar):
+        return tf.Session().run(tf.gradients(tf.gradients(nll_value(mu, Htt, Ztt, W, ttbar), mu), mu))
+
     a = second_derivative(mu, Htt, Ztt, W, ttbar)
     y = f(x, a, 1)
     
 
     ####
-    #### only call this function, if there is no .csv file containing dnll-values
-    ####
-
-    x_nll = np.linspace(0., 2., 30)
-    create_dnll_file(1.0, x_nll, Htt, Ztt, W, ttbar)
-
-
-    ####
-    #### assign values from .csv file
-    ####
-
-    diff_nll = scan_from_file(x)
-
-
-    ####
     #### Plot data
     ####
 
-    plt.figure()
-    #plt.plot(x_nll, diff_nll)
-    plt.plot(x, y, color='k')
-    plt.xlabel("mu")
-    plt.xlim((0, 2))
-    plt.ylabel("-2 Delta NLL")
-    plt.ylim((0, 9))
-    #plt.axvline(x= 1. - sigma_left, ymax=1. / 9., color='r')
-    #plt.axvline(x= 1. + sigma_right, ymax=1. / 9., color='r')
-    #plt.axhline(y=1., xmin=0., xmax=(1.-sigma_left) / 2., color='r')
-    #plt.axhline(y=1., xmin=(1.+sigma_right) / 2., xmax=2. / 2., color='r')
-    plt.axhline(y=1., color='r')
-    #plt.savefig("./scan_cross_check.png", bbox_inches="tight")
-    plt.savefig(os.path.join(args.workdir, 'model_fold{}/scan_cross_check.png'.format(args.fold)), bbox_inches="tight")
+    ## Plot settings
+    y_limit = [0.0, 4.5]
+    x_limit = [0.5, 1.5]
+    linewidth_narrow = 1.
+    linewidth_wide = 2.
+
+    plt.figure(figsize=(7,6))
+    plt.xlabel("$\mu$")
+    plt.xlim((x_limit[0], x_limit[1]))
+    plt.ylabel("-2 $\cdot \/ \Delta$NLL")
+    plt.ylim((y_limit[0], y_limit[1]))
+    plt.plot(x_new, f_dnll_array, color='C0', lw=linewidth_wide)
+    plt.plot(x, y, color='C1', lw=linewidth_wide)
+    plt.plot([x_limit[0], constraints_xval[0]], [1, 1], 'k', lw=linewidth_narrow)
+    plt.plot([constraints_xval[1], x_limit[1]], [1, 1], 'k', lw=linewidth_narrow)
+    vscale = 1 / y_limit[1]
+    plt.axvline(x=constraints_xval[0], ymax=1. * vscale, color='C0', lw=linewidth_narrow)
+    plt.axvline(x=constraints_xval[1], ymax=1. * vscale, color='C0', lw=linewidth_narrow)
+    plt.plot([0], [0], color='C0', label="$\mu_{}$         = 1.00 (-{:.3f} +{:.3f})".format('\mathrm{stat.}', constraints[1], constraints[0]))
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=1, mode="expand", borderaxespad=0., prop={'size': 14})
+    plt.savefig(os.path.join(args.workdir, 'model_fold{}/scan_cross_check{}.png'.format(args.fold, args.fold)), bbox_inches="tight")
 
 
 if __name__ == '__main__':
