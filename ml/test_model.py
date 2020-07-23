fixed scaling of mig01 to 1

ml/test_model.py
@@ -85,8 +85,8 @@ def main(args):
 
 
     # Magnify Mig01 to have a bigger impact on training
-    mean_value = np.mean(mig01)
-    mig01 = (mig01 - mean_value) * 10 + mean_value
+    #mean_value = np.mean(mig01)
+    #mig01 = (mig01 - mean_value) * 10 + mean_value
 
     # Process Mig01 to have same number of entries as other variables
     mig01 = np.append(mig01, np.ones(len(w) - len(mig01)))