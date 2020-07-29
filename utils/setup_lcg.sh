559396cc61b12a94ff6779db903d7264b761a9db
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2020-07-29 12:31:59 +0200

    enable training on deepthought

--- utils/setup_lcg.sh
+++ utils/setup_lcg.sh
@@ -2,10 +2,23 @@
 
 # TODO: Still still links to our local build, to be fixed as soon as ROOT 6.20 is on LCG
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
-source /home/wunsch/workspace/root/build_own_python/bin/thisroot.sh
+if uname -a | grep ekpdeepthought -q
+then
+    export CUDA_VISIBLE_DEVICES='3'
+    source /home/wunsch/workspace/py3_venv_ubuntu/bin/activate
+    source /home/wunsch/workspace/root/build_ubuntu/bin/thisroot.sh
+    export PATH=/usr/local/cuda-8.0/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
+    export PATH=/usr/local/cuda-9.0/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH
+    export PATH=/usr/local/cuda-10.0/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
+else
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
+    source /home/wunsch/workspace/root/build_own_python/bin/thisroot.sh
 
-alias python=/home/wunsch/workspace/python/install/bin/python3.6
+    alias python=/home/wunsch/workspace/python/install/bin/python3.6
+    export PYTHONPATH=/home/wunsch/.local/lib/python3.6/site-packages/:$PYTHONPATH
+fi
 export PYTHONPATH=$PWD:$PYTHONPATH
-export PYTHONPATH=/home/wunsch/.local/lib/python3.6/site-packages/:$PYTHONPATH
 export PYTHONPATH=$PWD/Dumbledraw:$PYTHONPATH
