2cf06603da0bf75e1aa6dbffe729a4372b1dd8ad
gesris <gessi.risto@gmx.de>
2020-08-05 16:02:51 +0200

    automatically generated arbitrary commit message for convenience

--- ml/train.sh
+++ ml/train.sh
@@ -7,5 +7,5 @@ WORKDIR=$1
 rm -rf $WORKDIR/model_fold?
 mkdir -p $WORKDIR/model_fold0
 mkdir -p $WORKDIR/model_fold1
-python ml/train.py $WORKDIR 0
-python ml/train.py $WORKDIR 1
+python3 ml/train.py $WORKDIR 0
+python3 ml/train.py $WORKDIR 1
