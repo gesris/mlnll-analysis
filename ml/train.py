f682da3fa3596da66a0e7aba0e95428b168f19df
gesris <gessi.risto@gmx.de>
2020-08-10 14:50:26 +0200

    automatically generated arbitrary commit message for convenience

--- ml/train.py
+++ ml/train.py
@@ -170,7 +170,8 @@ def main(args):
     nuisance_param = {}
     epsilon = tf.constant(1e-9, tf.float64)
     zero = tf.constant(0.0, tf.float64)
-    bincontent = []
+    signal = []
+    background = []
     for i, (up, down) in enumerate(zip(bins[1:], bins[:-1])):
         logger.debug('Add NLL for bin {} with boundaries [{}, {}]'.format(i, down, up))
         up = tf.constant(up, tf.float64)
@@ -199,10 +200,12 @@ def main(args):
         sig = 0
         for p in ['ggh', 'qqh']:
             sig += procs[p]
+        signal.append(sig)
 
         bkg = 0
         for p in ['ztt', 'zl', 'w', 'tt', 'vv', 'qcd']:
             bkg += procs[p]
+        background.append(bkg)
 
         # Bin by bin uncertainties
         shift = 0.0
@@ -219,8 +222,6 @@ def main(args):
 
         # Likelihood
         nll -= tfp.distributions.Poisson(tf.maximum(exp, epsilon)).log_prob(tf.maximum(obs, epsilon))
-    bincontent.append(sig)
-    bincontent.append(bkg)
     # Nuisance constraints
     for n in nuisance_param:
         nll -= tfp.distributions.Normal(
@@ -274,10 +275,11 @@ def main(args):
             minimize = minimize_fullnll
             is_warmup = False
 
-        loss_train, _, procs_ = session.run([loss, minimize, bincontent],
+        loss_train, _, signal_, background_ = session.run([loss, minimize, signal, background],
                 feed_dict={x_ph: x_train_preproc, y_ph: y_train, w_ph: w_train})
 
-        logger.info("\n{}".format(procs_))
+        logger.info("\n{}".format(signal_))
+        logger.info("\n{}".format(background_))
         #for i in range(8):
         #    logger.info("\n\nBIN {}:".format(i))
         #    for entry in procs_:
