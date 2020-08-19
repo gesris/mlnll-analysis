631ac6989e722f00982b4860221ca001bea1d3d0
gesris <gessi.risto@gmx.de>
2020-08-19 14:22:36 +0200

    automatically generated arbitrary commit message for convenience

--- ml.sh
+++ ml.sh
@@ -5,10 +5,10 @@ WORKDIR=$1
 #mkdir -p $WORKDIR
 
 # Create training dataset
-#ml/dataset.sh $WORKDIR
+ml/dataset.sh $WORKDIR
 
 # Train model
-#ml/train.sh $WORKDIR
+ml/train.sh $WORKDIR
 
 # Validate model
 # TODO: Missing any validation!
