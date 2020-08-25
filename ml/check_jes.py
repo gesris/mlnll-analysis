8393cac20c10ec50e8be8735ea8c9ee4a8d25b10
gesris <gessi.risto@gmx.de>
2020-08-25 17:21:00 +0200

    automatically generated arbitrary commit message for convenience

--- ml/check_jes.py
+++ ml/check_jes.py
@@ -5,15 +5,24 @@ path = '/work/gristo/second_mlnll-analysis/output/8_bins_nosysimpl_shapes/cmb/co
 f = ROOT.TFile(path)
 d = f.Get('htt_mt_0_2018')
 
-tot_jes_hist = ROOT.TH1F("tot_jes_hist", "", 8, 0, 1)
-entries = []
+tot_jes_upshift = ROOT.TH1F("tot_jes_upshift", "", 8, 0, 1)
+jes_upshift = []
 for key in d.GetListOfKeys():
     name = key.GetName()
     if 'Up' in name:
         h = d.Get(name)
-        tot_jes_hist.Add(h)
+        tot_jes_upshift.Add(h)
+
+tot_jes_downshift = ROOT.TH1F("tot_jes_downshift", "", 8, 0, 1)
+jes_downshift = []
+for key in d.GetListOfKeys():
+    name = key.GetName()
+    if 'Down' in name:
+        h = d.Get(name)
+        tot_jes_downshift.Add(h)
 
 for i in range(1, 9):
-    entries.append(tot_jes_hist.GetBinContent(i))
+    jes_upshift.append(tot_jes_upshift.GetBinContent(i))
+    jes_downshift.append(tot_jes_downshift.GetBinContent(i))
 
-print(entries)
+print("UPSHIFT: {} \nDOWNSHIFT: {}".format(jes_upshift, jes_downshift))
