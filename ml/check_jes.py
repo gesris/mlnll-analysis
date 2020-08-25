4f48b482c33f6344472d049f03f8a23472bede39
gesris <gessi.risto@gmx.de>
2020-08-25 18:14:28 +0200

    automatically generated arbitrary commit message for convenience

--- ml/check_jes.py
+++ ml/check_jes.py
@@ -4,11 +4,9 @@ import numpy as np
 path = '/work/gristo/second_mlnll-analysis/output/8_bins_nosysimpl_shapes/cmb/common/htt_input_2018.root'
 f = ROOT.TFile(path)
 d = f.Get('htt_mt_0_2018')
-
 classes = ['W', 'ZTT', 'ZL', 'ZJ', 'TTT', 'TTL', 'TTJ', 'VVJ', 'VVT', 'VVL', 'ggH125', 'qqH125']
 
-diff_hist = ROOT.TH1F("tot_jes_upshift", "", 8, 0, 1)
-
+diff_hist = ROOT.TH1F("DIFF", "", 8, 0, 1)
 tot_jes_upshift = ROOT.TH1F("tot_jes_upshift", "", 8, 0, 1)
 jes_upshift = []
 for key in d.GetListOfKeys():
@@ -19,8 +17,9 @@ for key in d.GetListOfKeys():
                 h_nom = d.Get(class_name)
                 h_shift = d.Get(name)
                 # subtract hists to get shift only
-                tot_jes_upshift.Add(h_nom, h_shift, -1, 1)
-                print(class_name, name)
+                diff_hist.Add(h_nom, h_shift, -1, 1)
+                tot_jes_upshift.Add(diff_hist)
+
 
 tot_jes_downshift = ROOT.TH1F("tot_jes_downshift", "", 8, 0, 1)
 jes_downshift = []
@@ -32,7 +31,9 @@ for key in d.GetListOfKeys():
                 h_nom = d.Get(class_name)
                 h_shift = d.Get(name)
                 # subtract hists to get shift only
-                tot_jes_downshift.Add(h_nom, h_shift, -1, 1)
+                diff_hist.Add(h_nom, h_shift, -1, 1)
+                tot_jes_downshift.Add(diff_hist)
+
 
 tot_sig_bkg = ROOT.TH1F("tot_sig_bkg", "", 8, 0, 1)
 sig_bkg = []
@@ -48,6 +49,7 @@ for key in d.GetListOfKeys():
         h = d.Get(name)
         tot_sig_bkg.Add(h)
 
+
 for i in range(1, 9):
     jes_upshift.append(tot_jes_upshift.GetBinContent(i))
     jes_downshift.append(tot_jes_downshift.GetBinContent(i))
@@ -56,23 +58,5 @@ for i in range(1, 9):
 print("UPSHIFT: {} \nSUM: {}".format(jes_upshift,np.sum(jes_upshift)))
 print("DOWNSHIFT: {} \nSUM: {}".format(jes_downshift,np.sum(jes_downshift)))
 print("SIG + BKG: {} \nSUM: {}".format(sig_bkg,np.sum(sig_bkg)))
-
-
-
-diff_hist = ROOT.TH1F("DIFF", "", 8, 0, 1)
-ztt_hist = d.Get('W')
-ztt_shift_hist = d.Get('W_CMS_scale_j_BBEC1Down')
-diff_hist.Add(ztt_hist, ztt_shift_hist, -1, 1)
-
-ztt=[]
-ztt_shift=[]
-diff=[]
-for i in range(1, 9):
-    ztt.append(ztt_hist.GetBinContent(i))
-    ztt_shift.append(ztt_shift_hist.GetBinContent(i))
-    diff.append(diff_hist.GetBinContent(i))
-print("ZTT: {}".format(ztt))
-print("ZTT Shift: {}".format(ztt_shift))
-print("DIFF: {}".format(diff))
 
 
