f596a84a72ada456f1fd7f8154a32920f2b5c158
gesris <gessi.risto@gmx.de>
2020-08-27 11:35:37 +0200

    automatically generated arbitrary commit message for convenience

--- ml/check_jes.py
+++ ml/check_jes.py
@@ -8,12 +8,10 @@ d = f.Get('htt_mt_0_2018')
 classes = ['W', 'ZTT', 'ZL', 'ZJ', 'TTT', 'TTL', 'TTJ', 'VVJ', 'VVT', 'VVL', 'ggH125', 'qqH125']
 
 
-## Calculating total upshift
+## Calculation total up- and downshift in root hist
 diff_hist = ROOT.TH1F("DIFF", "", 8, 0, 1)
 tot_jes_upshift = ROOT.TH1F("tot_jes_upshift", "", 8, 0, 1)
 tot_jes_downshift = ROOT.TH1F("tot_jes_downshift", "", 8, 0, 1)
-jes_upshift = []
-jes_downshift = []
 upshifts = {}
 downshifts = {}
 for key in d.GetListOfKeys():
@@ -49,46 +47,18 @@ for key in d.GetListOfKeys():
                     nom_array.append(h_nom.GetBinContent(i))
                 downshifts[name] = np.square(np.array(shift_array) - np.array(nom_array))
 
+## Transferring root hists to np arrays
 tot_upshifts = [0, 0, 0, 0, 0, 0, 0, 0]
 for h in upshifts:
     tot_upshifts += upshifts[h]
 tot_upshifts = np.sqrt(np.abs(np.array(tot_upshifts)))
 print("UPSHIFT:   {}".format(tot_upshifts))
+
 tot_downshifts = [0, 0, 0, 0, 0, 0, 0, 0]
 for h in downshifts:
     tot_downshifts += downshifts[h]
 tot_downshifts = np.sqrt(np.abs(np.array(tot_downshifts)))
 print("DOWNSHIFT: {}".format(tot_downshifts))
-
-
-"""
-## Calculating total downshift
-downshifts = {}
-tot_jes_downshift = ROOT.TH1F("tot_jes_downshift", "", 8, 0, 1)
-jes_downshift = []
-for key in d.GetListOfKeys():
-    name = key.GetName()
-    if 'Down' in name:
-        for class_name in classes:
-            if class_name in name:
-                h_nom = d.Get(class_name)
-                h_shift = d.Get(name)
-                # subtract hists to get shift only
-                diff_hist.Add(h_nom, h_shift, 1, -1)
-                tot_jes_downshift.Add(diff_hist)
-
-                shift_array = []
-                nom_array = []
-                for i in range(1, 9):
-                    shift_array.append(h_shift.GetBinContent(i))
-                    nom_array.append(h_nom.GetBinContent(i))
-                downshifts[name] = np.square(np.array(shift_array) - np.array(nom_array))
-tot_downshifts = [0, 0, 0, 0, 0, 0, 0, 0]
-for h in downshifts:
-    tot_downshifts += downshifts[h]
-tot_downshifts = np.sqrt(np.abs(np.array(tot_downshifts)))
-print("DOWNSHIFT: {}".format(tot_downshifts))
-"""
 
 
 ## Writing new histograms with total up- and downshift
@@ -113,58 +83,6 @@ for key in d.GetListOfKeys():
             newhdown.SetBinContent(i, tot_downshifts[i - 1])
 
         ## Write content
-        newhup.Write()
-        newhdown.Write()
+        newhup.Write('htt_mt_0_2018')
+        newhdown.Write('htt_mt_0_2018')
 
-
-
-
-
-
-"""
-tot_sig_bkg = ROOT.TH1F("tot_sig_bkg", "", 8, 0, 1)
-sig_bkg = []
-procs = {}
-for key in d.GetListOfKeys():
-    name = key.GetName()
-    if 'Up' in name:
-        pass
-    elif 'Down' in name:
-        pass
-    elif 'data_obs' in name:
-        pass
-    else:
-        h = d.Get(name)
-        tot_sig_bkg.Add(h)
-
-        placeholder_array = []
-        for i in range(1, 9):
-            placeholder_array.append(h.GetBinContent(i))
-        procs[name] = placeholder_array
-
-
-for i in range(1, 9):
-    jes_upshift.append(tot_jes_upshift.GetBinContent(i))
-    jes_downshift.append(tot_jes_downshift.GetBinContent(i))
-    sig_bkg.append(tot_sig_bkg.GetBinContent(i))
-
-print("UPSHIFT: {} \nSUM: {}".format(jes_upshift,np.sum(jes_upshift)))
-print("DOWNSHIFT: {} \nSUM: {}".format(jes_downshift,np.sum(jes_downshift)))
-print("SIG + BKG: {} \nSUM: {}".format(sig_bkg,np.sum(sig_bkg)))
-
-
-Signal = ['ggH125', 'qqH125']
-Background = ['W', 'ZTT', 'ZL', 'ZJ', 'TTT', 'TTL', 'TTJ', 'VVJ', 'VVT', 'VVL', 'QCD']
-
-## Calculate NLL
-bins = np.array(cfg.analysis_binning)
-for i, (up, down) in enumerate(zip(bins[1:], bins[:-1])):
-    sig = 0
-    for p in Signal:
-        sig += procs[p]
-    
-    bkg = 0
-    for p in Background:
-        bkg += procs[p]
-    
-"""
