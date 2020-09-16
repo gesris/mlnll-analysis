70c929c6440f77bf2958511435d36a71c814f3c9
gesris <gessi.risto@gmx.de>
2020-09-16 14:09:45 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_weight.py
+++ ml/jes_weight.py
@@ -6,4 +6,8 @@ from utils import config as cfg
 
 for name in cfg.files:
     for path in cfg.files[name]:
-        print(cfg.basepath + path)
+        f = ROOT.TFile(cfg.basepath + path, 'update')
+        d = f.Get()
+        for key in d.GetListOfKeys():
+            name = key.GetName()
+            print(name)
