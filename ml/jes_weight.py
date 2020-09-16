6bb8bee4047ac73ce7350d4be2f18a6ef161a9fc
gesris <gessi.risto@gmx.de>
2020-09-16 17:44:55 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_weight.py
+++ ml/jes_weight.py
@@ -6,18 +6,18 @@ from utils import config as cfg
 
 upshifts = {}
 
+df = ROOT.RDataFrame('mt_jecUncRelativeBalUp/ntuple', '/ceph/htautau/deeptau_02-20/2018/ntuples/GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2/GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2.root')
+hist = df.Histo1D('jpt_1')
+print(hist.GetBinContent(2))
+
+"""
 f = ROOT.TFile('/ceph/htautau/deeptau_02-20/2018/ntuples/GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2/GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2.root')
-for key in f.GetListOfKeys():
-    name = key.GetName()
-    if 'mt_jecUnc' in name:
-        if 'Up' in name:
-            d = f.Get(name)
-            tree = d.Get('ntuple')
-            for entry in tree:
-                x = entry.jpt_1
-                print(x)
-        elif 'Down' in name:
-            pass
+d = f.Get('mt_jecUncRelativeBalUp')
+tree = d.Get('ntuple')
+for entry in tree:
+    x = entry.jpt_1
+    print(x)
+"""
 """
 for name in cfg.files:
     for path in cfg.files[name]:
