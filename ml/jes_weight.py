ba8fce7a8dffb04df27334da3032e8375cbf6fc1
gesris <gessi.risto@gmx.de>
2020-09-16 21:33:13 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_weight.py
+++ ml/jes_weight.py
@@ -19,7 +19,8 @@ dir_up = df_up.AsNumpy(columns=["jpt_1"])    # hist is now a dictionary with ent
 print(dir_up["jpt_1"])
 
 hist_nominal = df_nominal.Histo1D("jpt_1")
-hist_nominal_rebinned = hist_nominal.Rebin(10, 0, 800)
+xbins = array.array('d', [3,4,5,6,7,8,10,12,15,20])
+hist_nominal_rebinned = hist_nominal.Rebin(10, '', xbins)
 hist_up = df_up.Histo1D("jpt_1")
 
 hist_nominal.Merge(hist_up.GetPtr())
