c7ee8fb6facd6a34029c271c66580c56cf4c8965
gesris <gessi.risto@gmx.de>
2020-09-16 16:38:17 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_weight.py
+++ ml/jes_weight.py
@@ -4,14 +4,11 @@ from utils import config as cfg
 
 # cfg.basepath + cfg.files = root file location
 
-pfad = '/ceph/htautau/deeptau_02-20/2018/ntuples/GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2/GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2.root'
-f = ROOT.TFile(pfad, 'update')
 
-
-#for name in cfg.files:
-    #for path in cfg.files[name]:
-        #f = ROOT.TFile(cfg.basepath + path + '/' + path + '.root', 'update')
-        #d = f.Get(path)
-        #for key in d.GetListOfKeys():
-        #    name = key.GetName()
-        #    print(name)
+for name in cfg.files:
+    for path in cfg.files[name]:
+        f = ROOT.TFile(cfg.basepath + path + '/' + path + '.root')
+        d = f.Get(path)
+        for key in d.GetListOfKeys():
+            name = key.GetName()
+            print(name)
