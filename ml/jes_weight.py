eab9a47d813e8558c11e183c8a4ddab11359d3a1
gesris <gessi.risto@gmx.de>
2020-09-16 14:04:12 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_weight.py
+++ ml/jes_weight.py
@@ -2,5 +2,8 @@ import ROOT
 import numpy as np
 from utils import config as cfg
 
+# cfg.basepath + cfg.files = root file location
+
 for name in cfg.files:
-    print(name, cfg.files[name])
+    for path in cfg.files[name]:
+        print(cfg.basepath + path)
