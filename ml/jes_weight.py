d196be84cc75a87a04db15cc9bb5162f3c7520fc
gesris <gessi.risto@gmx.de>
2020-09-17 10:39:39 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_weight.py
+++ ml/jes_weight.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 #mpl.rc("font", size=16, family="serif")
 
 # cfg.basepath + cfg.files = root file location
-
+"""
 path = '/ceph/htautau/deeptau_02-20/2018/ntuples/GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2/GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2.root'
 df_nominal = ROOT.RDataFrame('mt_nominal/ntuple', path)
 df_up = ROOT.RDataFrame('mt_jecUncRelativeBalUp/ntuple', path)
@@ -42,22 +42,60 @@ plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", bo
 plt.xlabel("jpt_1")
 plt.ylabel("Counts")
 plt.savefig('/home/gristo/workspace/plots/test_hist.png', bbox_inches = "tight")
-
-
-
-
 """
+
+
+
+
 for name in cfg.files:
-    for file in cfg.files[name]:
-        path = cfg.basepath + 'ntuples/' + file + '/' + file + '.root'
-        f = ROOT.TFile(path)
-        #d = f.Get(path)
-        for key in f.GetListOfKeys():
-            name = key.GetName()
-            if 'mt_jecUnc' in name:
-                if 'Up' in name:
-                    pass                    
-                elif 'Down' in name:
-                    pass
-"""
+    if 'ggh' in name:
+        tot_upshift = np.zeros(10)
+        tot_downshift = np.zeros(10)
+
+        for file in cfg.files[name]:
+            path = cfg.basepath + 'ntuples/' + file + '/' + file + '.root'
+            df_nominal = ROOT.RDataFrame('mt_nominal/ntuple', path)
+            nominal = ROOT.RDataFrame('mt_nominal/ntuple', path).AsNumpy(["jpt_1"])
+            heights_nom, bins = np.histogram(nominal["jpt_1"], bins=10, range=(-10, 800))
+            
+            f = ROOT.TFile(path)
+            for key in f.GetListOfKeys():
+                name = key.GetName()
+
+                if 'mt_jecUnc' in name:
+                    if 'Up' in name:
+                        df_up = ROOT.RDataFrame(name + '/ntuple', path)
+                        upshift = ROOT.RDataFrame('mt_jecUncRelativeBalUp/ntuple', path).AsNumpy(["jpt_1"])
+                        heights_up, _ = np.histogram(upshift["jpt_1"], bins=10, range=(-10, 800))
+                        
+                        ## SUM Of SQUARE DIFF
+                        tot_upshift += np.square(heights_up - heights_nom)
+
+
+                    elif 'Down' in name:
+                        df_down = ROOT.RDataFrame(name + '/ntuple', path)   
+                        downshift = ROOT.RDataFrame('mt_jecUncRelativeBalDown/ntuple', path).AsNumpy(["jpt_1"])
+                        heights_down, _ = np.histogram(downshift["jpt_1"], bins=10, range=(-10, 800))           
+                        
+                        ## SUM Of SQUARE DIFF
+                        tot_downshift += np.square(heights_nom - heights_down)
+
+        tot_upshift = np.square(tot_upshift)
+        tot_downshift = np.square(tot_downshift)
+
+        bins_center = []
+        for left, right in zip(bins[1:], bins[:-1]):
+            bins_center.append(left + (right - left) / 2)
+
+        plt.figure(figsize=(7, 6))
+        plt.hist(bins_center, weights=heights_nom, bins=bins, histtype="step", lw=1.5, color='C0')
+        plt.hist(bins_center, weights=heights_nom + tot_upshift, bins=bins, histtype="step", lw=1.5, ls=':', color='C1')
+        plt.hist(bins_center, weights=heights_nom - tot_downshift, bins=bins, histtype="step", lw=1.5, ls='--', color='C1')
+        plt.plot([0], [0], lw=2, color='C0', label="nominal")
+        plt.plot([0], [0], lw=2, ls=':', color='C1', label="up shift")
+        plt.plot([0], [0], lw=2, ls='--', color='C1', label="down shift")
+        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
+        plt.xlabel("jpt_1")
+        plt.ylabel("Counts")
+        plt.savefig('/home/gristo/workspace/plots/test_hist.png', bbox_inches = "tight")
 
