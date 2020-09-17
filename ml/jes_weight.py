81395e29f0a9572c2d93f4d154d2bb221d3be29d
gesris <gessi.risto@gmx.de>
2020-09-17 13:24:00 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_weight.py
+++ ml/jes_weight.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 
 for filename in cfg.files:
-    if 'qqh' in filename:
+    if 'vv' in filename:
         print(filename)
         tot_nom = np.zeros(10)
         tot_upshift = np.zeros(10)
