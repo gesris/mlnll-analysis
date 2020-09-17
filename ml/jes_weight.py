a23d94f6e66ec14a5903b525c33386768769669e
gesris <gessi.risto@gmx.de>
2020-09-17 12:04:26 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_weight.py
+++ ml/jes_weight.py
@@ -48,59 +48,59 @@ plt.savefig('/home/gristo/workspace/plots/test_hist.png', bbox_inches = "tight")
 
 
 for filename in cfg.files:
-    #if 'ggh' in filename:
-    print(filename)
-    tot_nom = np.zeros(10)
-    tot_upshift = np.zeros(10)
-    tot_downshift = np.zeros(10)
-    n = 0
+    if 'wjets' in filename:
+        print(filename)
+        tot_nom = np.zeros(10)
+        tot_upshift = np.zeros(10)
+        tot_downshift = np.zeros(10)
+        n = 0
 
-    for file in cfg.files[filename]:
-        print(file)
-        path = cfg.basepath + 'ntuples/' + file + '/' + file + '.root'
-        nominal = ROOT.RDataFrame('mt_nominal/ntuple', path).AsNumpy(["jpt_1"])
-        heights_nom, bins = np.histogram(nominal["jpt_1"], bins=10, range=(-10, 800))
+        for file in cfg.files[filename]:
+            print(file)
+            path = cfg.basepath + 'ntuples/' + file + '/' + file + '.root'
+            nominal = ROOT.RDataFrame('mt_nominal/ntuple', path).AsNumpy(["jpt_1"])
+            heights_nom, bins = np.histogram(nominal["jpt_1"], bins=10, range=(-10, 800))
 
-        ## SUM OF SQUARES
-        tot_nom += heights_nom
-        n += 1
-        
-        f = ROOT.TFile(path)
-        for key in f.GetListOfKeys():
-            name = key.GetName()
+            ## SUM OF SQUARES
+            tot_nom += heights_nom
+            n += 1
+            
+            f = ROOT.TFile(path)
+            for key in f.GetListOfKeys():
+                name = key.GetName()
 
-            if 'mt_jecUnc' in name:
-                if 'Up' in name:
-                    upshift = ROOT.RDataFrame(name + '/ntuple', path).AsNumpy(["jpt_1"])
-                    heights_up, _ = np.histogram(upshift["jpt_1"], bins=10, range=(-10, 800))
-                    
-                    ## SUM Of SQUARE DIFF
-                    tot_upshift += np.square(heights_up - heights_nom)
+                if 'mt_jecUnc' in name:
+                    if 'Up' in name:
+                        upshift = ROOT.RDataFrame(name + '/ntuple', path).AsNumpy(["jpt_1"])
+                        heights_up, _ = np.histogram(upshift["jpt_1"], bins=10, range=(-10, 800))
+                        
+                        ## SUM Of SQUARE DIFF
+                        tot_upshift += np.square(heights_up - heights_nom)
 
-                elif 'Down' in name: 
-                    downshift = ROOT.RDataFrame(name + '/ntuple', path).AsNumpy(["jpt_1"])
-                    heights_down, _ = np.histogram(downshift["jpt_1"], bins=10, range=(-10, 800))           
-                    
-                    ## SUM Of SQUARE DIFF
-                    tot_downshift += np.square(heights_nom - heights_down)
+                    elif 'Down' in name: 
+                        downshift = ROOT.RDataFrame(name + '/ntuple', path).AsNumpy(["jpt_1"])
+                        heights_down, _ = np.histogram(downshift["jpt_1"], bins=10, range=(-10, 800))           
+                        
+                        ## SUM Of SQUARE DIFF
+                        tot_downshift += np.square(heights_nom - heights_down)
 
-    tot_nom = tot_nom / n
-    tot_upshift = np.sqrt(tot_upshift)
-    tot_downshift = np.sqrt(tot_downshift)
+        tot_nom = tot_nom / n
+        tot_upshift = np.sqrt(tot_upshift)
+        tot_downshift = np.sqrt(tot_downshift)
 
-    bins_center = []
-    for left, right in zip(bins[1:], bins[:-1]):
-        bins_center.append(left + (right - left) / 2)
+        bins_center = []
+        for left, right in zip(bins[1:], bins[:-1]):
+            bins_center.append(left + (right - left) / 2)
 
-    plt.figure(figsize=(7, 6))
-    plt.hist(bins_center, weights=tot_nom, bins=bins, histtype="step", lw=1.5, color='C0')
-    plt.hist(bins_center, weights=tot_nom + tot_upshift, bins=bins, histtype="step", lw=1.5, ls=':', color='C1')
-    plt.hist(bins_center, weights=tot_nom - tot_downshift, bins=bins, histtype="step", lw=1.5, ls='--', color='C1')
-    plt.plot([0], [0], lw=2, color='C0', label="nominal")
-    plt.plot([0], [0], lw=2, ls=':', color='C1', label="up shift")
-    plt.plot([0], [0], lw=2, ls='--', color='C1', label="down shift")
-    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
-    plt.xlabel("jpt_1")
-    plt.ylabel("Counts")
-    plt.savefig('/home/gristo/workspace/plots/jpt1_totshift_{}.png'.format(filename), bbox_inches = "tight")
+        plt.figure(figsize=(7, 6))
+        plt.hist(bins_center, weights=tot_nom, bins=bins, histtype="step", lw=1.5, color='C0')
+        plt.hist(bins_center, weights=tot_nom + tot_upshift, bins=bins, histtype="step", lw=1.5, ls=':', color='C1')
+        plt.hist(bins_center, weights=tot_nom - tot_downshift, bins=bins, histtype="step", lw=1.5, ls='--', color='C1')
+        plt.plot([0], [0], lw=2, color='C0', label="nominal")
+        plt.plot([0], [0], lw=2, ls=':', color='C1', label="up shift")
+        plt.plot([0], [0], lw=2, ls='--', color='C1', label="down shift")
+        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
+        plt.xlabel("jpt_1")
+        plt.ylabel("Counts")
+        plt.savefig('/home/gristo/workspace/plots/jpt1_totshift_{}.png'.format(filename), bbox_inches = "tight")
 
