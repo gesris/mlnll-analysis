813f360abf22a28933cbd0cfc25872454f155db9
gesris <gessi.risto@gmx.de>
2020-09-18 13:32:17 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_weight.py
+++ ml/jes_weight.py
@@ -68,7 +68,7 @@ for filename in cfg.files:
                 #heights_nom = np.array(heights_nom)
                 #heights_up = np.array(heights_up)
                 #heights_down = np.array(heights_down)
-                print(heights_nom)
+                print(heights_nom[heights_nom == 0])
                 heights_up[heights_up == 0] = epsilon
                 heights_down[heights_down == 0] = epsilon
                 heights_nom[heights_nom == 0] = epsilon
