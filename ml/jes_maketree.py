44a79d635df45500b9f6d263789d7d1ed8a3f408
gesris <gessi.risto@gmx.de>
2020-09-19 11:29:29 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_maketree.py
+++ ml/jes_maketree.py
@@ -25,16 +25,22 @@ for filename in cfg.files:
                 binning = load_from_csv(home_basepath + file_ , '/binning.csv')
                 weights_up = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_up.csv'.format(file_))
                 weights_down = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_down.csv'.format(file_))
-                a = np.array(weights_up, dtype=[('jpt_1_weights_up', np.float32)])
-                b = np.array(weights_down, dtype=[('jpt_1_weights_down', np.float32)])
+                #a = np.array(weights_up, dtype=[('jpt_1_weights_up', np.float32)])
+                #b = np.array(weights_down, dtype=[('jpt_1_weights_down', np.float32)])
 
                 ## Make tree with two branches upweights and downweights
                 root_file = ROOT.TFile(home_basepath + file_ + '/jpt_1_weights.root', 'RECREATE')
                 tree = ROOT.TTree('tree', 'jpt_1_weights')
-                x = np.array(weights_up, dtype='float32')
-                y = np.array(weights_down, dtype='float32')
+                x = np.zeros(1, dtype=float)
+                y = np.zeros(1, dtype=float)
                 tree.Branch('jpt_1_weights_up', x, 'jpt_1_weights_up/F')
                 tree.Branch('jpt_1_weights_down', y, 'jpt_1_weights_down/F')
-                tree.Fill()
+
+                ## Fill tree
+                for i in range(len(weights_down)):
+                    x[0] = weights_up[i]
+                    y[0] = weights_down[i]
+                    tree.Fill()
                 root_file.Write()
+                root_file.Close()
 
