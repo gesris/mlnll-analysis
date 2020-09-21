762070998a5f8a97c4d3bd81284f2571f12d2180
gesris <gessi.risto@gmx.de>
2020-09-21 13:14:15 +0200

    automatically generated arbitrary commit message for convenience

--- ml/jes_maketree.py
+++ ml/jes_maketree.py
@@ -19,37 +19,32 @@ def load_from_csv(path, filename):
 
 
 for filename in cfg.files:
-    if filename in ['ggh']:
-        for file_ in cfg.files[filename]:
-            if file_ in ['GluGluHToTauTauHTXSFilterSTXS1p1Bin101M125_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v2']:
-                binning = load_from_csv(home_basepath + file_ , '/binning.csv')
-                weights_up = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_up.csv'.format(file_))
-                weights_down = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_down.csv'.format(file_))
+    for file_ in cfg.files[filename]:
+        binning = load_from_csv(home_basepath + file_ , '/binning.csv')
+        weights_up = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_up.csv'.format(file_))
+        weights_down = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_down.csv'.format(file_))
 
-                
-                ## Make new root file with new tree with two branches upweights and downweights
-                root_file = ROOT.TFile(home_basepath + file_ + '/jpt_1_weights.root', 'RECREATE')
-                tree = ROOT.TTree('tree', 'jpt_1_weights')
+        
+        ## Make new root file with new tree with two branches upweights and downweights
+        root_file = ROOT.TFile(home_basepath + file_ + '/jpt_1_weights.root', 'RECREATE')
+        tree = ROOT.TTree('jpt_1', 'jpt_1')
 
-                ## create 1 dimensional float arrays as fill variables, in this way the float
-                ## array serves as a pointer which can be passed to the branch
-                x = array('f', [0])
-                y = array('f', [0])
-                #x = np.zeros(1, dtype=float)
-                #y = np.zeros(1, dtype=float)
+        ## create 1 dimensional float arrays as fill variables, in this way the float
+        ## array serves as a pointer which can be passed to the branch
+        x = array('f', [0])
+        y = array('f', [0])
 
-                ## create the branches and assign the fill-variables to them as floats (F)
-                tree.Branch('jpt_1_weights_up', x, 'jpt_1_weights_up/F')
-                tree.Branch('jpt_1_weights_down', y, 'jpt_1_weights_down/F')
-                
-                
-                ## Fill tree
-                for i in range(len(weights_down)):
-                    x[0] = weights_up[i]
-                    y[0] = weights_down[i]
-                    tree.Fill()
-                #tree.Fill()
+        ## create the branches and assign the fill-variables to them as floats (F)
+        tree.Branch('jpt_1_weights_up', x, 'jpt_1_weights_up/F')
+        tree.Branch('jpt_1_weights_down', y, 'jpt_1_weights_down/F')
+        
+        
+        ## Fill tree
+        for i in range(len(weights_down)):
+            x[0] = weights_up[i]
+            y[0] = weights_down[i]
+            tree.Fill()
 
-                root_file.Write()
-                root_file.Close()
-                
+        root_file.Write()
+        root_file.Close()
+            
