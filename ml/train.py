d43f3d954f8f886f2e4271f0b3fa89081d703e7d
gesris <gessi.risto@gmx.de>
2020-09-23 18:46:25 +0200

    automatically generated arbitrary commit message for convenience

--- ml/train.py
+++ ml/train.py
@@ -295,7 +295,7 @@ def main(args):
 
         loss_train, _, procs_, procs_up_ = session.run([loss, minimize, procs, procs_up],
                 feed_dict={x_ph: x_train_preproc, y_ph: y_train, w_ph: w_train, scale_ph: scale_train, jpt_1_upshift_ph: jpt_1_upshift_train, jpt_1_downshift_ph: jpt_1_downshift_train})
-        logger.info("\n\nUP: {}\nNOM: {}".format(procs_['ggh'], procs_up_['ggh']))
+        logger.info("\n\nUP: {}\nNOM: {}\nDIFF: {}".format(procs_['ggh'], procs_up_['ggh'], procs_up_['ggh'] - procs_['ggh']))
         ## Breakup condition
         if is_warmup:
             loss_val = session.run(loss, feed_dict={x_ph: x_val_preproc, y_ph: y_val, w_ph: w_val, scale_ph: scale_val, jpt_1_upshift_ph: jpt_1_upshift_val, jpt_1_downshift_ph: jpt_1_downshift_val})
