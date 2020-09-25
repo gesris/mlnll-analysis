118b444381c6737c472df220faf2c7602bdc4ad7
gesris <gessi.risto@gmx.de>
2020-09-25 20:52:13 +0200

    automatically generated arbitrary commit message for convenience

--- utils/setup_lcg.sh
+++ utils/setup_lcg.sh
@@ -5,8 +5,8 @@
 # Logfile to check for available GPUs
 #touch available_gpu.log
 
-#if uname -a | grep ekpdeepthought -q
-#then
+if uname -a | grep ekpdeepthought -q
+then
 #    X=$(nvidia-smi | grep -n ' 0%' | head -1 | cut -f 1 -d ':')
 #    if [ $X = 9 ]
 #    then
@@ -33,21 +33,21 @@
 #        echo False > available_gpu.log
 #    fi
 
-#    export CUDA_VISIBLE_DEVICES=$GPU
-export CUDA_VISIBLE_DEVICES=2
-source /home/gristo/workspace/py3_venv_ubuntu/bin/activate
-source /home/wunsch/workspace/root/build_ubuntu/bin/thisroot.sh
-export PATH=/usr/local/cuda-8.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
-export PATH=/usr/local/cuda-9.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH
-export PATH=/usr/local/cuda-10.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
-#else
-#    source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
-#    source /home/wunsch/workspace/root/build_own_python/bin/thisroot.sh
-#    alias python=/home/wunsch/workspace/python/install/bin/python3.6
-#    export PYTHONPATH=/home/gristo/.local/lib/python3.6/site-packages/:$PYTHONPATH
-#fi
+    #export CUDA_VISIBLE_DEVICES=$GPU
+    export CUDA_VISIBLE_DEVICES=2
+    source /home/gristo/workspace/py3_venv_ubuntu/bin/activate
+    source /home/wunsch/workspace/root/build_ubuntu/bin/thisroot.sh
+    export PATH=/usr/local/cuda-8.0/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
+    export PATH=/usr/local/cuda-9.0/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH
+    export PATH=/usr/local/cuda-10.0/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
+else
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
+    source /home/wunsch/workspace/root/build_own_python/bin/thisroot.sh
+    alias python=/home/wunsch/workspace/python/install/bin/python3.6
+    export PYTHONPATH=/home/gristo/.local/lib/python3.6/site-packages/:$PYTHONPATH
+fi
 export PYTHONPATH=$PWD:$PYTHONPATH
 export PYTHONPATH=$PWD/Dumbledraw:$PYTHONPATH
