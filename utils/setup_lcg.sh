b5017b22431cfcb8ea5a183d64f4147aa340a4de
gesris <gessi.risto@gmx.de>
2020-09-25 19:25:34 +0200

    automatically generated arbitrary commit message for convenience

--- utils/setup_lcg.sh
+++ utils/setup_lcg.sh
@@ -34,20 +34,20 @@
 #    fi
 
 #    export CUDA_VISIBLE_DEVICES=$GPU
-    export CUDA_VISIBLE_DEVICES=2
-    source /home/gristo/workspace/py3_venv_ubuntu/bin/activate
-    source /home/wunsch/workspace/root/build_ubuntu/bin/thisroot.sh
-    export PATH=/usr/local/cuda-8.0/bin:$PATH
-    export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
-    export PATH=/usr/local/cuda-9.0/bin:$PATH
-    export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH
-    export PATH=/usr/local/cuda-10.0/bin:$PATH
-    export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
-else
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
-    source /home/wunsch/workspace/root/build_own_python/bin/thisroot.sh
-    alias python=/home/wunsch/workspace/python/install/bin/python3.6
-    export PYTHONPATH=/home/gristo/.local/lib/python3.6/site-packages/:$PYTHONPATH
-fi
+export CUDA_VISIBLE_DEVICES=2
+source /home/gristo/workspace/py3_venv_ubuntu/bin/activate
+source /home/wunsch/workspace/root/build_ubuntu/bin/thisroot.sh
+export PATH=/usr/local/cuda-8.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
+export PATH=/usr/local/cuda-9.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH
+export PATH=/usr/local/cuda-10.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
+#else
+#    source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
+#    source /home/wunsch/workspace/root/build_own_python/bin/thisroot.sh
+#    alias python=/home/wunsch/workspace/python/install/bin/python3.6
+#    export PYTHONPATH=/home/gristo/.local/lib/python3.6/site-packages/:$PYTHONPATH
+#fi
 export PYTHONPATH=$PWD:$PYTHONPATH
 export PYTHONPATH=$PWD/Dumbledraw:$PYTHONPATH
