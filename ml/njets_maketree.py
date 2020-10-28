385f0a605b1a95eca33f93e206de96e18638801d
gesris <gessi.risto@gmx.de>
2020-10-28 20:43:18 +0100

    bugfixes

--- ml/njets_maketree.py
+++ ml/njets_maketree.py
@@ -59,75 +59,75 @@ foldernames = [
 def job(filename):
     for file_ in cfg.files[filename]:
         print(file_)
-        #if file_ in 'SingleMuon_Run2018A_17Sep2018v2_13TeV_MINIAOD':
-        jpt1_binning = load_from_csv(home_basepath + file_ , '/jpt1_binning.csv')
-        njets_binning = load_from_csv(home_basepath + file_ , '/njets_binning.csv')
-        jpt1_weights_up = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_up.csv'.format(file_))
-        jpt1_weights_down = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_down.csv'.format(file_))
-        njets_weights_up = load_from_csv(home_basepath + file_ , '/{}_njets_weights_up.csv'.format(file_))
-        njets_weights_down = load_from_csv(home_basepath + file_ , '/{}_njets_weights_down.csv'.format(file_))
+        if file_ in 'W1JetsToLNu_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_madgraph-pythia8_v2':
+            jpt1_binning = load_from_csv(home_basepath + file_ , '/jpt1_binning.csv')
+            njets_binning = load_from_csv(home_basepath + file_ , '/njets_binning.csv')
+            jpt1_weights_up = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_up.csv'.format(file_))
+            jpt1_weights_down = load_from_csv(home_basepath + file_ , '/{}_jpt1_weights_down.csv'.format(file_))
+            njets_weights_up = load_from_csv(home_basepath + file_ , '/{}_njets_weights_up.csv'.format(file_))
+            njets_weights_down = load_from_csv(home_basepath + file_ , '/{}_njets_weights_down.csv'.format(file_))
 
-        
-        ## Make new root file with new tree with two branches upweights and downweights
-        root_file = ROOT.TFile(home_basepath + file_ + '/' + file_ + '.root', 'RECREATE')
-        tdirectory = ROOT.TDirectoryFile('mt_nominal', 'mt_nominal')
-        tdirectory.cd()
-        tree = ROOT.TTree('ntuple', 'ntuple')
-
-
-        ## create 1 dimensional float arrays as fill variables, in this way the float
-        ## array serves as a pointer which can be passed to the branch
-        jpt1_x = array('f', [0])
-        jpt1_y = array('f', [0])
-        njets_x = array('f', [0])
-        njets_y = array('f', [0])
+            
+            ## Make new root file with new tree with two branches upweights and downweights
+            root_file = ROOT.TFile(home_basepath + file_ + '/' + file_ + '.root', 'RECREATE')
+            tdirectory = ROOT.TDirectoryFile('mt_nominal', 'mt_nominal')
+            tdirectory.cd()
+            tree = ROOT.TTree('ntuple', 'ntuple')
 
 
-        ## create the branches and assign the fill-variables to them as floats (F)
-        tree.Branch('jpt_1_weights_up', jpt1_x, 'jpt_1_weights_up/F')
-        tree.Branch('jpt_1_weights_down', jpt1_y, 'jpt_1_weights_down/F')
-        tree.Branch('njets_weights_up', njets_x, 'njets_weights_up/F')
-        tree.Branch('njets_weights_down', njets_y, 'njets_weights_down/F')
+            ## create 1 dimensional float arrays as fill variables, in this way the float
+            ## array serves as a pointer which can be passed to the branch
+            jpt1_x = array('f', [0])
+            jpt1_y = array('f', [0])
+            njets_x = array('f', [0])
+            njets_y = array('f', [0])
 
 
-        ## Loading basepath root files to match weight with event
-        path = cfg.basepath + 'ntuples/' + file_ + '/' + file_ + '.root'
-        nominal = ROOT.TFile(path)
-        tree_2 = nominal.Get("mt_nominal/ntuple")
-        
+            ## create the branches and assign the fill-variables to them as floats (F)
+            tree.Branch('jpt_1_weights_up', jpt1_x, 'jpt_1_weights_up/F')
+            tree.Branch('jpt_1_weights_down', jpt1_y, 'jpt_1_weights_down/F')
+            tree.Branch('njets_weights_up', njets_x, 'njets_weights_up/F')
+            tree.Branch('njets_weights_down', njets_y, 'njets_weights_down/F')
 
-        ## assigning specific weight to each event
-        ## NJETS
-        for event in tree_2:
-            if event.njets > njets_binning[-2]:   #all entries over value of left bin edge of last bin are ignored
-                ## assign weight 1 to entries out of bounds
-                njets_x[0] = 1.
-                njets_y[0] = 1.
-                tree.Fill()
-            else:
-                left_binedge = njets_binning[njets_binning <= event.njets][-1]
-                index = np.where(njets_binning==left_binedge)
-                print(left_binedge)
-                njets_x[0] = njets_weights_up[index][0]
-                njets_y[0] = njets_weights_down[index][0]
-                tree.Fill()
-        
-        ## JPT1
-        for event in tree_2:
-            if event.jpt_1 > jpt1_binning[-1]:
-                jpt1_x[0] = 1.
-                jpt1_y[0] = 1.
-                tree.Fill()
-            else:
-                left_binedge = jpt1_binning[jpt1_binning <= event.jpt_1][-1]
-                index = np.where(jpt1_binning==left_binedge)
-                print(left_binedge)
-                jpt1_x[0] = jpt1_weights_up[index][0]
-                jpt1_y[0] = jpt1_weights_down[index][0]
-                tree.Fill()
-        
-        root_file.Write()
-        root_file.Close()
+
+            ## Loading basepath root files to match weight with event
+            path = cfg.basepath + 'ntuples/' + file_ + '/' + file_ + '.root'
+            nominal = ROOT.TFile(path)
+            tree_2 = nominal.Get("mt_nominal/ntuple")
+            
+
+            ## assigning specific weight to each event
+            ## NJETS
+            for event in tree_2:
+                if event.njets > njets_binning[-2]:   #all entries over value of left bin edge of last bin are ignored
+                    ## assign weight 1 to entries out of bounds
+                    njets_x[0] = 1.
+                    njets_y[0] = 1.
+                    tree.Fill()
+                else:
+                    left_binedge = njets_binning[njets_binning <= event.njets][-1]
+                    index = np.where(njets_binning==left_binedge)
+                    print(left_binedge)
+                    njets_x[0] = njets_weights_up[index][0]
+                    njets_y[0] = njets_weights_down[index][0]
+                    tree.Fill()
+            
+            ## JPT1
+            for event in tree_2:
+                if event.jpt_1 > jpt1_binning[-1]:
+                    jpt1_x[0] = 1.
+                    jpt1_y[0] = 1.
+                    tree.Fill()
+                else:
+                    left_binedge = jpt1_binning[jpt1_binning <= event.jpt_1][-1]
+                    index = np.where(jpt1_binning==left_binedge)
+                    print(left_binedge)
+                    jpt1_x[0] = jpt1_weights_up[index][0]
+                    jpt1_y[0] = jpt1_weights_down[index][0]
+                    tree.Fill()
+            
+            root_file.Write()
+            root_file.Close()
 
 
 def clone_to_all_tdirectories(tdirectories):
@@ -135,18 +135,19 @@ def clone_to_all_tdirectories(tdirectories):
         for filename in cfg.files:
             print(filename)
             for file_ in cfg.files[filename]:
-                ## Loadng TDirectory needet to clone
-                f = ROOT.TFile(home_basepath + file_ + '/' + file_ + '.root', 'UPDATE')
-                t = f.Get("mt_nominal/ntuple")
+                if file_ in 'W1JetsToLNu_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_madgraph-pythia8_v2':
+                    ## Loadng TDirectory needet to clone
+                    f = ROOT.TFile(home_basepath + file_ + '/' + file_ + '.root', 'UPDATE')
+                    t = f.Get("mt_nominal/ntuple")
 
-                ## Making new TDirectory
-                d_new = ROOT.TDirectoryFile(folder, folder)
-                d_new.cd()
+                    ## Making new TDirectory
+                    d_new = ROOT.TDirectoryFile(folder, folder)
+                    d_new.cd()
 
-                ## Cloning and Saving changes
-                tree_clone = t.Clone()
-                d_new.Write()
-                f.Close()
+                    ## Cloning and Saving changes
+                    tree_clone = t.Clone()
+                    d_new.Write()
+                    f.Close()
 
 
 ## With multiprozessing with 1 core per category
@@ -154,8 +155,8 @@ if __name__=="__main__":
     filenames = []
     for filename in cfg.files:
         filenames.append(filename)
-    p = mp.Pool(len(filenames))
-    #p = mp.Pool(1)
+    #p = mp.Pool(len(filenames))
+    p = mp.Pool(1)
     p.map(job, filenames)
     p.close()
     p.join()
--- ml/njets_weights.py
+++ ml/njets_weights.py
@@ -26,72 +26,76 @@ def load_from_csv(path, filename):
 
 for filename in cfg.files:
     print(filename)
-    #if filename in 'vv':
-    for file_ in cfg.files[filename]:
-        print(file_)
-        #if file_ in ['ZZ_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_pythia8_v2']:
-        ## Make directory for Hist and .csv with weights
-        if os.path.exists(home_basepath + file_):
-            print("Directory {} exists".format(home_basepath + file_))
-        else:
-            try:
-                os.mkdir(home_basepath + file_)
-            except OSError:
-                print("Creating directory {} failed".format(home_basepath + file_))
-            else:
-                print("Successfully created directory %s " % [home_basepath + file_])
-        
-        ## Loading root files
-        path = cfg.basepath + 'ntuples/' + file_ + '/' + file_ + '.root'
-        nominal = ROOT.RDataFrame('mt_nominal/ntuple', path).AsNumpy(["njets"])
-        
-        ## Prepatre for Hist
-        bins = 10
-        minrange = 0
-        maxrange = 10
-        binning = np.linspace(minrange, maxrange, bins + 1)
-        heights_nom, bins = np.histogram(nominal["njets"], bins=bins, range=(minrange, maxrange))
-        
-        ## Calculate shifts
-        ## Upshift: add 20% of first bin on first bin and substract same amount from second bin
-        ## Downshift: addsubtract 20% of first bin from first bin and add same amount on second bin
-        diff = heights_nom[0] * 0.2
-        heights_up = np.append([heights_nom[0] + diff, heights_nom[1] - diff], heights_nom[2:])
-        heights_down = np.append([heights_nom[0] - diff, heights_nom[1] + diff], heights_nom[2:])
+    if filename in 'wjets':
+        for file_ in cfg.files[filename]:
+            print(file_)
+            if file_ in ['W1JetsToLNu_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_madgraph-pythia8_v2']:
+                ## Make directory for Hist and .csv with weights
+                if os.path.exists(home_basepath + file_):
+                    print("Directory {} exists".format(home_basepath + file_))
+                else:
+                    try:
+                        os.mkdir(home_basepath + file_)
+                    except OSError:
+                        print("Creating directory {} failed".format(home_basepath + file_))
+                    else:
+                        print("Successfully created directory %s " % [home_basepath + file_])
+                
+                ## Loading root files
+                path = cfg.basepath + 'ntuples/' + file_ + '/' + file_ + '.root'
+                nominal = ROOT.RDataFrame('mt_nominal/ntuple', path).AsNumpy(["njets"])
+                
+                ## Prepatre for Hist
+                bins = 10
+                minrange = 0
+                maxrange = 10
+                binning = np.linspace(minrange, maxrange, bins + 1)
+                heights_nom, bins = np.histogram(nominal["njets"], bins=bins, range=(minrange, maxrange))
+                
+                ## Calculate shifts
+                ## Upshift: add 20% of first bin on first bin and substract same amount from second bin
+                ## Downshift: addsubtract 20% of first bin from first bin and add same amount on second bin
+                diff = heights_nom[0] * 0.2
+                heights_up = np.append([heights_nom[0] + diff, heights_nom[1] - diff], heights_nom[2:])
+                heights_down = np.append([heights_nom[0] - diff, heights_nom[1] + diff], heights_nom[2:])
 
-        ## Calculate weights
-        epsilon = 1e-6
-        heights_nom = heights_nom.astype(float)
-        heights_nom[heights_nom == 0] = epsilon
+                ## Calculate weights
+                epsilon = 1e-6
+                heights_nom = heights_nom.astype(float)
+                heights_up = heights_up.astype(float)
+                heights_down = heights_down.astype(float)
+                heights_nom[heights_nom <= 0] = epsilon
+                heights_up[heights_up <= 0] = epsilon
+                heights_down[heights_down <= 0] = epsilon
 
-        weights_up = heights_up / heights_nom
-        weights_down = heights_down / heights_nom
+                weights_up = heights_up / heights_nom
+                weights_down = heights_down / heights_nom
 
-        weights_up[weights_up == 0] = epsilon
-        weights_down[weights_down == 0] = epsilon
+                weights_up[weights_up <= 0] = epsilon
+                weights_down[weights_down <= 0] = epsilon
 
-        ## Save weights to .csv
-        #save_to_csv(weights_up, home_basepath + file_, '/{}_njets_weights_up.csv'.format(file_))
-        np.savetxt(home_basepath + file_ + '/{}_njets_weights_up.csv'.format(file_), np.asarray(weights_up), delimiter=',')
-        np.savetxt(home_basepath + file_ + '/{}_njets_weights_down.csv'.format(file_), np.asarray(weights_down), delimiter=',')
-        np.savetxt(home_basepath + file_ + '/njets_binning.csv', np.asarray(binning), delimiter=',')
+                ## Save weights to .csv
+                #save_to_csv(weights_up, home_basepath + file_, '/{}_njets_weights_up.csv'.format(file_))
+                np.savetxt(home_basepath + file_ + '/{}_njets_weights_up.csv'.format(file_), np.asarray(weights_up), delimiter=',')
+                np.savetxt(home_basepath + file_ + '/{}_njets_weights_down.csv'.format(file_), np.asarray(weights_down), delimiter=',')
+                np.savetxt(home_basepath + file_ + '/njets_binning.csv', np.asarray(binning), delimiter=',')
 
-        
-        ## Make Histogram
-        bins_center = []
-        for left, right in zip(bins[1:], bins[:-1]):
-            bins_center.append(left + (right - left) / 2)
+                
+                ## Make Histogram
+                bins_center = []
+                for left, right in zip(bins[1:], bins[:-1]):
+                    bins_center.append(left + (right - left) / 2)
 
-        plt.figure(figsize=(7, 6))
-        plt.hist(bins_center, weights=heights_nom, bins=bins, histtype="step", lw=1.5, color='C0')
-        plt.hist(bins_center, weights=heights_up, bins=bins, histtype="step", lw=1.5, ls=':', color='C1')
-        plt.hist(bins_center, weights=heights_down, bins=bins, histtype="step", lw=1.5, ls='--', color='C1')
-        plt.plot([0], [0], lw=2, color='C0', label="nominal")
-        plt.plot([0], [0], lw=2, ls=':', color='C1', label="up shift")
-        plt.plot([0], [0], lw=2, ls='--', color='C1', label="down shift")
-        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
-        plt.xlabel("njets")
-        plt.ylabel("Counts")
-        plt.savefig(home_basepath + file_ + '/{}_njets_shapeshift.png'.format(file_), bbox_inches = "tight")
+                plt.figure(figsize=(7, 6))
+                plt.hist(bins_center, weights=heights_nom, bins=bins, histtype="step", lw=1.5, color='C0')
+                plt.hist(bins_center, weights=heights_up, bins=bins, histtype="step", lw=1.5, ls=':', color='C1')
+                plt.hist(bins_center, weights=heights_down, bins=bins, histtype="step", lw=1.5, ls='--', color='C1')
+                plt.plot([0], [0], lw=2, color='C0', label="nominal")
+                plt.plot([0], [0], lw=2, ls=':', color='C1', label="up shift")
+                plt.plot([0], [0], lw=2, ls='--', color='C1', label="down shift")
+                plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
+                plt.xlabel("njets")
+                plt.ylabel("Counts")
+                plt.savefig(home_basepath + file_ + '/{}_njets_shapeshift.png'.format(file_), bbox_inches = "tight")
         
                 
