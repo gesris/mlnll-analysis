c1648816b444d18c404e83f331cf23aa34eaaa42
gesris <gessi.risto@gmx.de>
2020-11-11 13:53:15 +0100

    bugfixes

--- ml/train.py
+++ ml/train.py
@@ -237,11 +237,11 @@ def main(args):
         nll -= tfp.distributions.Poisson(tf.maximum(exp, epsilon)).log_prob(tf.maximum(obs, epsilon))
     
     # Nuisance constraints
-    nuisances.append(n)
-    for n in nuisances:
-        nll -= tfp.distributions.Normal(
-                loc=tf.constant(0.0, dtype=tf.float64), scale=tf.constant(1.0, dtype=tf.float64)
-                ).log_prob(n)
+    #nuisances.append(n)
+    #for n in nuisances:
+    #    nll -= tfp.distributions.Normal(
+    #            loc=tf.constant(0.0, dtype=tf.float64), scale=tf.constant(1.0, dtype=tf.float64)
+    #            ).log_prob(n)
 
     # Compute constraint of mu
     def get_constraint(nll, params):
