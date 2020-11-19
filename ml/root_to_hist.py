b7c115e8b219d022dc5cac7d8d844cd2f54ba6a8
gesris <gessi.risto@gmx.de>
2020-11-19 17:11:13 +0100

    bugfixes

--- ml/root_to_hist.py
+++ ml/root_to_hist.py
@@ -14,66 +14,63 @@ home_basepath = "/home/gristo/workspace/jpt_1_plots/"
 
 for filename in cfg.files:
     print(filename)
-    if filename in 'tt':
+    if filename in ['vv', 'ggh', 'qqh']:
         for file_ in cfg.files[filename]:
             print(file_)
-            if file_ in ['TTToSemiLeptonic_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1']:
-                if os.path.exists(home_basepath + file_):
-                    print("Directory {} exists".format(home_basepath + file_))
+            #if file_ in ['TTToSemiLeptonic_RunIIAutumn18MiniAOD_102X_13TeV_MINIAOD_powheg-pythia8_v1']:
+            if os.path.exists(home_basepath + file_):
+                print("Directory {} exists".format(home_basepath + file_))
+            else:
+                try:
+                    os.mkdir(home_basepath + file_)
+                except OSError:
+                    print("Creating directory {} failed".format(home_basepath + file_))
                 else:
-                    try:
-                        os.mkdir(home_basepath + file_)
-                    except OSError:
-                        print("Creating directory {} failed".format(home_basepath + file_))
-                    else:
-                        print("Successfully created directory %s " % [home_basepath + file_])
+                    print("Successfully created directory %s " % [home_basepath + file_])
 
-                ## Loading root file
-                path = cfg.basepath + 'ntuples/' + file_ + '/' + file_ + '.root'
-                file = ROOT.TFile(path)
+            ## Loading root file
+            path = cfg.basepath + 'ntuples/' + file_ + '/' + file_ + '.root'
+            file = ROOT.TFile(path)
 
-                ## Prepare for hist
-                bins = 10
-                #minrange = -10
-                #maxrange = 800
-                minrange = 0
-                maxrange = 200
-                binning = np.linspace(minrange, maxrange, bins + 1)
-                nominal = ROOT.RDataFrame('mt_nominal/ntuple', path).AsNumpy(["jpt_1"])
-                heights_nom, _ = np.histogram(nominal["jpt_1"], bins=bins, range=(minrange, maxrange))
-                hist_upshifts = {}
-                hist_downshifts = {}
+            ## Prepare for hist
+            bins = 10
+            #minrange = -10
+            #maxrange = 800
+            minrange = 0
+            maxrange = 200
+            binning = np.linspace(minrange, maxrange, bins + 1)
+            nominal = ROOT.RDataFrame('mt_nominal/ntuple', path).AsNumpy(["jpt_1"])
+            heights_nom, _ = np.histogram(nominal["jpt_1"], bins=bins, range=(minrange, maxrange))
+            hist_upshifts = {}
+            hist_downshifts = {}
 
-                for key in file.GetListOfKeys():
-                    name = key.GetName()
-                    if 'mt_jecUnc' in name:
-                        print(name)
-                        if 'Up' in name:
-                            upshift = ROOT.RDataFrame(name + '/ntuple', path).AsNumpy(["jpt_1"])
-                            hist_upshifts[name], _ = np.histogram(upshift["jpt_1"], bins=bins, range=(minrange, maxrange))
-                            
-                        elif 'Down' in name: 
-                            downshift = ROOT.RDataFrame(name + '/ntuple', path).AsNumpy(["jpt_1"])
-                            hist_downshifts[name], _ = np.histogram(downshift["jpt_1"], bins=bins, range=(minrange, maxrange))           
-                            
-                ## Make histograms
-                bins_center = []
-                for left, right in zip(binning[1:], binning[:-1]):
-                    bins_center.append(left + (right - left) / 2)
-                
-                for uncertainty_up, uncertainty_down in zip(hist_upshifts, hist_downshifts):
-                    plt.figure(figsize=(7, 6))
-                    plt.hist(bins_center, weights=heights_nom, bins=bins, histtype="step", lw=1.5, color='C0')
-                    plt.hist(bins_center, weights=hist_upshifts[uncertainty_up], bins=bins, histtype="step", lw=1.5, ls=':', color='C1')
-                    plt.hist(bins_center, weights=hist_downshifts[uncertainty_down], bins=bins, histtype="step", lw=1.5, ls='--', color='C1')
-                    plt.plot([0], [0], lw=2, color='C0', label="nominal")
-                    plt.plot([0], [0], lw=2, ls=':', color='C1', label="up shift")
-                    plt.plot([0], [0], lw=2, ls='--', color='C1', label="down shift")
-                    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
-                    plt.xlabel("jpt_1")
-                    plt.ylabel("Counts")
-                    plt.savefig(home_basepath + file_+ '/' + '{}.png'.format(uncertainty_up), bbox_inches = "tight")
-
-
-
+            for key in file.GetListOfKeys():
+                name = key.GetName()
+                if 'mt_jecUnc' in name:
+                    print(name)
+                    if 'Up' in name:
+                        upshift = ROOT.RDataFrame(name + '/ntuple', path).AsNumpy(["jpt_1"])
+                        hist_upshifts[name], _ = np.histogram(upshift["jpt_1"], bins=bins, range=(minrange, maxrange))
+                        
+                    elif 'Down' in name: 
+                        downshift = ROOT.RDataFrame(name + '/ntuple', path).AsNumpy(["jpt_1"])
+                        hist_downshifts[name], _ = np.histogram(downshift["jpt_1"], bins=bins, range=(minrange, maxrange))           
+                        
+            ## Make histograms
+            bins_center = []
+            for left, right in zip(binning[1:], binning[:-1]):
+                bins_center.append(left + (right - left) / 2)
             
+            for uncertainty_up, uncertainty_down in zip(hist_upshifts, hist_downshifts):
+                plt.figure(figsize=(7, 6))
+                plt.hist(bins_center, weights=heights_nom, bins=bins, histtype="step", lw=1.5, color='C0')
+                plt.hist(bins_center, weights=hist_upshifts[uncertainty_up], bins=bins, histtype="step", lw=1.5, ls=':', color='C1')
+                plt.hist(bins_center, weights=hist_downshifts[uncertainty_down], bins=bins, histtype="step", lw=1.5, ls='--', color='C1')
+                plt.plot([0], [0], lw=2, color='C0', label="nominal")
+                plt.plot([0], [0], lw=2, ls=':', color='C1', label="up shift")
+                plt.plot([0], [0], lw=2, ls='--', color='C1', label="down shift")
+                plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0., prop={'size': 14})
+                plt.xlabel("jpt_1")
+                plt.ylabel("Counts")
+                plt.savefig(home_basepath + file_+ '/' + '{}.png'.format(uncertainty_up), bbox_inches = "tight")
+
