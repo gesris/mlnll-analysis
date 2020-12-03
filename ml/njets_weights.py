0b1ae152d63382254fca8d3e6658f5edc25b2116
gesris <gessi.risto@gmx.de>
2020-12-03 16:11:54 +0100

    different njets shift

--- ml/njets_weights.py
+++ ml/njets_weights.py
@@ -62,12 +62,11 @@ for filename in cfg.files:
                 ## New more advanced shift: add/subtract 1 to every event, except edges
                 ## Upshift
                 print("Events == 0: {}".format(len(nominal["njets"][nominal["njets"] == 0])))
-                print(nominal["njets"][nominal["njets"] == 0])
-                print("Events != 0: {}".format(len(nominal["njets"][nominal["njets"] != 0])))
-                print(nominal["njets"][nominal["njets"] != 0])
+                print("Events != 0 (BEFORE): {}".format(len(nominal["njets"][nominal["njets"] != 0])))
+
                 nominal["njets"][nominal["njets"] != 0] = nominal["njets"][nominal["njets"] != 0] - 1
-                print(len(nominal["njets"][nominal["njets"] != 0]))
-                print(nominal["njets"][nominal["njets"] != 0])
+                
+                print("Events != 0 (AFTER): {}".format(len(nominal["njets"][nominal["njets"] != 0])))
                 print("Tot events: {}".format(len(nominal["njets"])))
 
 
