95aa73a2cbc2786233ec2b21875d491880eebc5d
gesris <gessi.risto@gmx.de>
2020-12-08 16:23:38 +0100

    automatically generated arbitrary commit message for convenience

--- ml/collect_events.py
+++ ml/collect_events.py
@@ -75,10 +75,6 @@ def write_dataset(d, workdir, name, group, fold, weightstr, cutstr):
     for v in cfg.ml_variables:
         variables.push_back(v)
     variables.push_back(cfg.ml_weight)
-    #variables.push_back("jpt_1_weights_up")
-    #variables.push_back("jpt_1_weights_down")
-    # variables.push_back("njets_weights_up")
-    # variables.push_back("njets_weights_down")
     variables.push_back("met_weights_up")
     variables.push_back("met_weights_down")
     variables.push_back("m_vis_weights_up")
