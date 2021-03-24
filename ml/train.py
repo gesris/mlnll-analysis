642d3001af09ea89913de56649dc87a7580b2992
gesris <gessi.risto@gmx.de>
2021-03-24 12:29:58 +0100

    implemented bbb uncertainties

--- ml/train.py
+++ ml/train.py
@@ -144,7 +144,7 @@ def main(args):
     classes = cfg.ml_classes + [n + '_ss' for n in cfg.ml_classes if n not in ['ggh', 'qqh']] + ['data_ss']
     x, y, w, m_vis_upshift, m_vis_downshift, met_upshift, met_downshift = build_dataset(os.path.join(args.workdir, 'fold{}.root'.format(args.fold)), classes, args.fold,
                             use_class_weights=False, make_categorical=False)
-    x_train, x_val, y_train, y_val, w_train, w_val, m_vis_upshift_train, m_vis_upshift_val, m_vis_downshift_train, m_vis_downshift_val, met_upshift_train, met_upshift_val, met_downshift_train, met_downshift_val = train_test_split(x, y, w, m_vis_upshift, m_vis_downshift, met_upshift, met_downshift, test_size=0.25, random_state=1234)
+    x_train, x_val, y_train, y_val, w_train, w_val = train_test_split(x, y, w, test_size=0.25, random_state=1234)
     logger.info('Number of train/val events in nominal dataset: {} / {}'.format(x_train.shape[0], x_val.shape[0]))
 
     # Scale to expectation in the full dataset
@@ -187,20 +187,12 @@ def main(args):
     # Build NLL loss
     y_ph = tf.placeholder(tf.float64, shape=(None,))
     w_ph = tf.placeholder(tf.float64, shape=(None,))
-    m_vis_upshift_ph = tf.placeholder(tf.float64)
-    m_vis_downshift_ph = tf.placeholder(tf.float64)
-    met_upshift_ph = tf.placeholder(tf.float64)
-    met_downshift_ph = tf.placeholder(tf.float64)
 
     nll = 0.0
     bins = np.array(cfg.analysis_binning)
     mu = tf.constant(1.0, tf.float64)
-    n_m_vis = tf.constant(0.0, tf.float64)
-    n_met = tf.constant(0.0, tf.float64)
-    n_z_norm = tf.constant(0.0, tf.float64)
-    n_w_norm = tf.constant(0.0, tf.float64)
     nuisances = []
-    zero = tf.constant(0, tf.float64)
+    # zero = tf.constant(0, tf.float64)
     epsilon = tf.constant(1e-9, tf.float64)
     for i, (up, down) in enumerate(zip(bins[1:], bins[:-1])):
         logger.debug('Add NLL for bin {} with boundaries [{}, {}]'.format(i, down, up))
@@ -210,30 +202,12 @@ def main(args):
         # Processes
         mask = count_masking(f, up, down)
         procs = {}
-        procs_up_m_vis = {}
-        procs_down_m_vis = {}
-        procs_up_met = {}
-        procs_down_met = {}
-        procs_up_z_norm = {}
-        procs_down_z_norm = {}
-        procs_up_w_norm = {}
-        procs_down_w_norm = {}
+        procs_sumw2 = {}
 
         for j, name in enumerate(classes):
             proc_w = mask * tf.cast(tf.equal(y_ph, tf.constant(j, tf.float64)), tf.float64) * w_ph
-            proc_w_up_m_vis = mask * tf.cast(tf.equal(y_ph, tf.constant(j, tf.float64)), tf.float64) * w_ph * m_vis_upshift_ph
-            proc_w_down_m_vis = mask * tf.cast(tf.equal(y_ph, tf.constant(j, tf.float64)), tf.float64) * w_ph * m_vis_downshift_ph
-            proc_w_up_met = mask * tf.cast(tf.equal(y_ph, tf.constant(j, tf.float64)), tf.float64) * w_ph * met_upshift_ph
-            proc_w_down_met = mask * tf.cast(tf.equal(y_ph, tf.constant(j, tf.float64)), tf.float64) * w_ph * met_downshift_ph
             procs[name] = tf.reduce_sum(proc_w)
-            procs_up_m_vis[name] = tf.reduce_sum(proc_w_up_m_vis)
-            procs_down_m_vis[name] = tf.reduce_sum(proc_w_down_m_vis)
-            procs_up_met[name] = tf.reduce_sum(proc_w_up_met)
-            procs_down_met[name] = tf.reduce_sum(proc_w_down_met)
-            procs_up_z_norm[name] = procs[name] * 1.2
-            procs_down_z_norm[name] = procs[name] * 0.8
-            procs_up_w_norm[name] = procs[name] * 1.2
-            procs_down_w_norm[name] = procs[name] * 0.8
+            procs_sumw2[name] = tf.reduce_sum(tf.square(proc_w)) 
 
 
         # QCD estimation
@@ -251,22 +225,13 @@ def main(args):
         for p in ['ztt', 'zl', 'w', 'tt', 'vv', 'qcd']:
             bkg += procs[p]
 
-        # JES Uncertainty
-        sys = 0.0
-        for p in ['ggh', 'qqh', 'ztt', 'zl', 'w', 'tt', 'vv']:
-            Delta_up_m_vis = tf.maximum(n_m_vis, zero) * (procs_up_m_vis[p] - procs[p])
-            Delta_down_m_vis = tf.minimum(n_m_vis, zero) * (procs[p] - procs_down_m_vis[p])
-            Delta_up_met = tf.maximum(n_met, zero) * (procs_up_met[p] - procs[p])
-            Delta_down_met = tf.minimum(n_met, zero) * (procs[p] - procs_down_met[p])
-            sys += Delta_up_met + Delta_down_met + Delta_up_m_vis + Delta_down_m_vis
-        for p in ['ztt']:
-            Delta_up_norm = tf.maximum(n_z_norm, zero) * (procs_up_z_norm[p] - procs[p])
-            Delta_down_norm = tf.minimum(n_z_norm, zero) * (procs[p] - procs_down_z_norm[p])
-            sys += Delta_up_norm + Delta_down_norm
-        for p in ['w']:
-            Delta_up_w_norm = tf.maximum(n_w_norm, zero) * (procs_up_w_norm[p] - procs[p])
-            Delta_down_w_norm = tf.minimum(n_w_norm, zero) * (procs[p] - procs_down_w_norm[p])
-            sys += Delta_up_w_norm + Delta_down_w_norm
+
+        # Bin by bin uncertainties
+        sys = tf.constant(0.0, tf.float64)
+        for p in ['ztt', 'zl', 'w', 'tt', 'vv', 'qcd']:
+            n = tf.constant(0.0, tf.float64)
+            nuisances.append(n)
+            sys += n * tf.sqrt(procs_sumw2[p])
 
         # Expectations
         obs = sig + bkg
@@ -276,10 +241,6 @@ def main(args):
         nll -= tfp.distributions.Poisson(tf.maximum(exp, epsilon)).log_prob(tf.maximum(obs, epsilon))
     
     ## Nuisance constraints
-    nuisances.append(n_m_vis)
-    nuisances.append(n_met)
-    nuisances.append(n_z_norm)
-    nuisances.append(n_w_norm)
     for n in nuisances:
        nll -= tfp.distributions.Normal(
                loc=tf.constant(0.0, dtype=tf.float64), scale=tf.constant(1.0, dtype=tf.float64)
@@ -332,11 +293,11 @@ def main(args):
             is_warmup = False
 
         loss_train, _ = session.run([loss, minimize],
-                feed_dict={x_ph: x_train_preproc, y_ph: y_train, w_ph: w_train, met_upshift_ph: met_upshift_train, met_downshift_ph: met_downshift_train, m_vis_upshift_ph: m_vis_upshift_train, m_vis_downshift_ph: m_vis_downshift_train})
+                feed_dict={x_ph: x_train_preproc, y_ph: y_train, w_ph: w_train})
         if is_warmup:
-            loss_val = session.run(loss, feed_dict={x_ph: x_val_preproc, y_ph: y_val, w_ph: w_val, met_upshift_ph: met_upshift_val, met_downshift_ph: met_downshift_val, m_vis_upshift_ph: m_vis_upshift_val, m_vis_downshift_ph: m_vis_downshift_val})
+            loss_val = session.run(loss, feed_dict={x_ph: x_val_preproc, y_ph: y_val, w_ph: w_val})
         else:
-            loss_val = session.run(loss, feed_dict={x_ph: x_val_preproc, y_ph: y_val, w_ph: w_val, met_upshift_ph: met_upshift_val, met_downshift_ph: met_downshift_val, m_vis_upshift_ph: m_vis_upshift_val, m_vis_downshift_ph: m_vis_downshift_val})
+            loss_val = session.run(loss, feed_dict={x_ph: x_val_preproc, y_ph: y_val, w_ph: w_val})
             if min_loss > loss_val and np.abs(min_loss - loss_val) / min_loss > tolerance:
                 min_loss = loss_val
                 patience_count = patience
@@ -353,7 +314,7 @@ def main(args):
         if step % validation_steps == 0:
             logger.info('Step / patience: {} / {}'.format(step, patience_count))
             logger.info('Train loss: {:.5f}'.format(loss_train))
-            loss_val = session.run(loss, feed_dict={x_ph: x_val_preproc, y_ph: y_val, w_ph: w_val, met_upshift_ph: met_upshift_val, met_downshift_ph: met_downshift_val, m_vis_upshift_ph: m_vis_upshift_val, m_vis_downshift_ph: m_vis_downshift_val})
+            loss_val = session.run(loss, feed_dict={x_ph: x_val_preproc, y_ph: y_val, w_ph: w_val})
             logger.info('Validation loss: {:.5f}'.format(loss_val))
             # path = saver.save(session, os.path.join(args.workdir, 'model_fold{}/model.ckpt'.format(args.fold)), global_step=step)
             # logger.info('Save model to {}'.format(path))
@@ -372,7 +333,8 @@ def main(args):
     plt.plot(steps_list, loss_train_list)
     plt.plot(steps_list, loss_val_list)
     plt.xlabel("Steps")
-    plt.ylabel("Loss (Train: {:.3f}, Val.: {:.3f})".format(loss_train_list[-1], loss_val_list[-1]))
+    plt.ylabel("Loss")# (Train: {:.3f}, Val.: {:.3f})".format(loss_train_list[-1], loss_val_list[-1]))
+    plt.legend( prop={'size': 11})
     plt.savefig(os.path.join(args.workdir, 'model_fold{}/minimization_fold{}.png'.format(args.fold, args.fold)), bbox_inches = "tight")
 
 
