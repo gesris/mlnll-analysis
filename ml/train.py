correct bbb calculation with consideration of statistically weighted events

ml/train.py
@@ -198,6 +198,10 @@ def main(args):
         procs = {}
         procs_up = {}
         procs_down = {}
+        procs_noweight = {}
+        scale_bbb = 0.0
+        sum_weighted = 0.0
+        sum_nonweighted = 0.0
 
         for j, name in enumerate(classes):
             proc_w = mask * tf.cast(tf.equal(y_ph, tf.constant(j, tf.float64)), tf.float64) * w_ph
@@ -206,6 +210,14 @@ def main(args):
             procs[name] = tf.reduce_sum(proc_w)
             procs_up[name] = tf.reduce_sum(proc_w_up)
             procs_down[name] = tf.reduce_sum(proc_w_down)
+
+            # BBB
+            proc_noweight = mask * tf.cast(tf.equal(y_ph, tf.constant(j, tf.float64)), tf.float64)
+            procs_noweight[name] = tf.reduce_sum(proc_noweight) 
+            sum_weighted += procs[name]
+            sum_nonweighted += procs_noweight[name]
+        scale_bbb += sum_weighted / sum_nonweighted
+
 
         # QCD estimation
         procs['qcd'] = procs['data_ss']
@@ -231,11 +243,11 @@ def main(args):
         
         # BBB Uncertainty
         bbb = tf.constant(0.0, tf.float64)
-        n = tf.constant(0.0, tf.float64)
+        n_bbb = tf.constant(0.0, tf.float64)
         for p in ['ztt', 'zl', 'w', 'tt', 'vv', 'qcd']:
-            bbb += procs[p]
-        sys += n * tf.sqrt(bbb)
-        nuisances.append(n)
+            bbb += procs_noweight[p]
+        sys += n_bbb * (tf.sqrt(bbb) * scale_bbb) 
+        nuisances.append(n_bbb)
 
         # Expectations
         obs = sig + bkg